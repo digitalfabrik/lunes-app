6aad3e8fea974f5d9baa6f816696efb0fddccde8
ayaShanaa <aya@kiitos-tech.com>
2021-01-06 19:14:19 +0200

    fix profession screen height

--- screens/ProfessionScreen/styles.ts
+++ screens/ProfessionScreen/styles.ts
@@ -4,8 +4,8 @@ import {COLORS} from '../../constants/colors';
 export const styles = StyleSheet.create({
   root: {
     backgroundColor: COLORS.lunesWhite,
-    height: 600,
-    paddingBottom: 50,
+    height: '100%',
+    paddingBottom: 0,
   },
   text: {
     textAlign: 'center',
