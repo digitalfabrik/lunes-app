b6524f4681f8e269d11e37cf01d133ac1ba32b87
ayaShanaa <aya@kiitos-tech.com>
2021-01-12 15:56:59 +0200

    install text-to-speech library

--- android/app/src/main/java/com/lunesappui/MainApplication.java
+++ android/app/src/main/java/com/lunesappui/MainApplication.java
@@ -4,6 +4,8 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import net.no_mad.tts.TextToSpeechPackage;
+import net.no_mad.tts.TextToSpeechPackage;
 import com.horcrux.svg.SvgPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
