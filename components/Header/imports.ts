9391b6974d9933a106598e109e1ac673494947a5
ayaShanaa <aya@kiitos-tech.com>
2021-01-12 14:56:46 +0200

    add all images' imports to images' imports file

--- components/Header/imports.ts
+++ components/Header/imports.ts
@@ -1,11 +1,13 @@
 import React from 'react';
 import {styles} from './styles';
 import {View} from 'react-native';
-import SquareIcon from '../../assets/images/icon-sign-6.svg';
-import StarIcon from '../../assets/images/icon-sign-1.svg';
-import CircleIcon from '../../assets/images/icon-sign-5.svg';
-import LinesIcon from '../../assets/images/icon-sign-3.svg';
-import SmileIcon from '../../assets/images/smile.svg';
+import {
+  SquareIcon,
+  StarIcon,
+  CircleIcon,
+  LinesIcon,
+  SmileIcon,
+} from '../../assets/images/imports';
 
 export {
   React,
--- components/ListItem/imports.ts
+++ components/ListItem/imports.ts
@@ -1,8 +1,7 @@
 import React, {useState} from 'react';
 import {styles} from './styles';
 import {IListItemProps} from '../../interfaces/profession';
-import Arrow from '../../assets/images/arrow.svg';
-import RedArrow from '../../assets/images/red-arrow.svg';
+import {Arrow, RedArrow} from '../../assets/images/imports';
 import {View, Text} from 'react-native';
 import {useFocusEffect} from '@react-navigation/native';
 
