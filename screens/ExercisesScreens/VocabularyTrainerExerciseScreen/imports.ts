529807067c4ca2ef1347743ad6fae1a98ed8be4a
ayaShanaa <aya@kiitos-tech.com>
2021-01-19 16:10:06 +0200

    fix imports

--- screens/ExercisesScreens/VocabularyTrainerExerciseScreen/imports.ts
+++ screens/ExercisesScreens/VocabularyTrainerExerciseScreen/imports.ts
@@ -1,16 +1,16 @@
 import React, {useState} from 'react';
 import {View, Text, TouchableOpacity, Image} from 'react-native';
 import {styles} from './styles';
-import {CloseButton, VolumeUpDisabled} from '../../assets/images/imports';
-import Modal from '../../components/Modal';
+import {CloseButton, VolumeUpDisabled} from '../../../assets/images/imports';
+import Modal from '../../../components/Modal';
 import {ProgressBar} from 'react-native-paper';
-import {COLORS} from '../../constants/colors';
-import {IDocumentProps} from '../../interfaces/exercise';
+import {COLORS} from '../../../constants/colors';
+import {IDocumentProps} from '../../../interfaces/exercise';
 import {useFocusEffect} from '@react-navigation/native';
-import axios from '../../utils/axios';
-import {ENDPOINTS} from '../../constants/endpoints';
-import {IVocabularyTrainerScreen} from '../../interfaces/exercise';
-import AnswerSection from '../../components/AnswerSection';
+import axios from '../../../utils/axios';
+import {ENDPOINTS} from '../../../constants/endpoints';
+import {IVocabularyTrainerScreen} from '../../../interfaces/exercise';
+import AnswerSection from '../../../components/AnswerSection';
 
 export {
   React,
