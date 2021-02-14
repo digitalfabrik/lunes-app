995f85553581066bdf3da99e45412878aeed5717
Aya Shanaa <ayashanaa@Ayas-MacBook-Pro.local>
2021-02-14 12:32:10 +0200

    fix navigation

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -1,3 +1,4 @@
+import {SCREENS} from '../constants/data';
 import {
   React,
   createStackNavigator,
@@ -21,10 +22,17 @@ import {
 
 const ProfessionStack = createStackNavigator<ProfessionParamList>();
 
-const defaultOptions = (title: string, Icon: any, navigation: any) => {
+const defaultOptions = (
+  title: string,
+  Icon: any,
+  navigation: any,
+  screen?: string,
+) => {
   return {
     headerLeft: () => (
-      <TouchableOpacity onPress={navigation.goBack} style={styles.headerLeft}>
+      <TouchableOpacity
+        onPress={screen ? () => navigation.navigate(screen) : navigation.goBack}
+        style={styles.headerLeft}>
         <Icon />
         <Text style={styles.title}>{title}</Text>
       </TouchableOpacity>
@@ -87,21 +95,36 @@ const Navigation = () => {
       />
       <ProfessionStack.Screen
         options={({navigation}) =>
-          defaultOptions('Results Overview', BackButton, navigation)
+          defaultOptions(
+            'Results Overview',
+            BackButton,
+            navigation,
+            SCREENS.ResultsOverview,
+          )
         }
         name="CorrectResults"
         component={CorrectResultsScreen}
       />
       <ProfessionStack.Screen
         options={({navigation}) =>
-          defaultOptions('Results Overview', BackButton, navigation)
+          defaultOptions(
+            'Results Overview',
+            BackButton,
+            navigation,
+            SCREENS.ResultsOverview,
+          )
         }
         name="AlmostCorrectResults"
         component={AlmostCorrectResultsScreen}
       />
       <ProfessionStack.Screen
         options={({navigation}) =>
-          defaultOptions('Results Overview', BackButton, navigation)
+          defaultOptions(
+            'Results Overview',
+            BackButton,
+            navigation,
+            SCREENS.ResultsOverview,
+          )
         }
         name="IncorrectResults"
         component={IncorrectResultsScreen}
