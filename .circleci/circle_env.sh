cd18c2a734aaa756e1c40dd5de0576e7cbd5a924
steffi <metzger@integreat-app.de>
2021-05-03 16:46:23 +0200

    LUN-28

--- .circleci/circle_env.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-function fetchKeystore {
-  sudo gpg --passphrase ${KEYSTORE_ENCRYPTION_KEY} --pinentry-mode loopback -o "android/app/$KEYSTORE_FILENAME" -d "android/$KEYSTORE_FILENAME.gpg"
-}
