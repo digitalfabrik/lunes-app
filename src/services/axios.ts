2a03af740ab19b7bf256b459e832286f1f697b37
steffi <metzger@integreat-app.de>
2021-08-03 16:24:30 +0200

    LUN-101: Use Test cms for local development

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,7 +1,13 @@
 import axios from 'axios'
 
-const baseURL = 'https://lunes.tuerantuer.org/api'
-// const baseURL = 'https://lunes-test.tuerantuer.org/api'
+
+let baseURL: string
+
+if (__DEV__) {
+  baseURL = 'https://lunes-test.tuerantuer.org/api'
+} else {
+  baseURL = 'https://lunes.tuerantuer.org/api'
+}
 
 const instance = axios.create({
   baseURL: baseURL
