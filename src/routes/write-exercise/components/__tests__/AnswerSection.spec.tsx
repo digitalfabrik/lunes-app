f72005a342a2c533a833bf4317c2f4a799683303
steffi <metzger@integreat-app.de>
2021-08-09 14:42:30 +0200

    LUN-102: fix tests

--- src/routes/write-exercise/components/__tests__/AnswerSection.spec.tsx
+++ src/routes/write-exercise/components/__tests__/AnswerSection.spec.tsx
@@ -3,6 +3,12 @@ import AnswerSection, { AnswerSectionPropsType } from '../AnswerSection'
 import { fireEvent, render } from '@testing-library/react-native'
 import labels from '../../../../constants/labels.json'
 
+jest.mock('../../../../components/AudioPlayer', () => {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const Text = require('react-native').Text
+  return () => <Text>AudioPlayer</Text>
+})
+
 describe('AnswerSection', () => {
   const defaultAnswerSectionProps: AnswerSectionPropsType = {
     documents: [
@@ -22,6 +28,13 @@ describe('AnswerSection', () => {
               id: 2,
               value: 'Die'
             }
+          },
+          {
+            word: 'Alternative',
+            article: {
+              id: 2,
+              value: 'Die'
+            }
           }
         ]
       }
@@ -34,23 +47,41 @@ describe('AnswerSection', () => {
     setCurrentDocumentNumber: () => {}
   }
 
-  /*  it('renders correctly across screens', () => {
-        const component = shallow(<AnswerSection {...defaultAnswerSectionProps} />)
-        expect(toJson(component)).toMatchSnapshot()
-      })
-
-      it('should render text input', () => {
-        const component = shallow(<AnswerSection {...defaultAnswerSectionProps} />)
-        expect(component.find('[testID="input-field"]')).toHaveLength(1)
-      })*/
-
-  it('should show correct for word', async () => {
-    const { findByPlaceholderText, findByRole, findByText } = render(<AnswerSection {...defaultAnswerSectionProps} />)
-    const input = await findByPlaceholderText(labels.exercises.write.insertAnswer)
-    // await fireEvent.changeText(input, {target: {value: 'Die Spachtel'}})
-    const button = await findByRole('button')
+  const evaluate = async (input: string, expectedFeedback: string) => {
+    const { getByPlaceholderText, getByText, getByTestId } = render(<AnswerSection {...defaultAnswerSectionProps} />)
+    const inputField = await getByPlaceholderText(labels.exercises.write.insertAnswer)
+    await fireEvent.changeText(inputField, input)
+    const button = await getByText(labels.exercises.write.checkInput)
     await fireEvent.press(button)
-    const result = findByText(labels.results.correct)
-    expect(result).toBeTruthy()
+    const result = await getByTestId('feedback-write-exercise')
+    expect(result.children[0]).toEqual(expectedFeedback)
+  }
+
+  it('should show correct-feedback for correct solution', async () => {
+    await evaluate('Der Spachtel', labels.exercises.write.feedback.correct)
+  })
+
+  it('should show correct-feedback for alternative article', async () => {
+    // const { getByPlaceholderText, getByText } = render(<AnswerSection {...defaultAnswerSectionProps} />)
+    await evaluate('Die Spachtel', labels.exercises.write.feedback.correct)
+  })
+
+  it('should show correct-feedback for alternative solution', async () => {
+    await evaluate('Die Alternative', labels.exercises.write.feedback.correct)
+  })
+
+  it('should show almost correct feedback', async () => {
+    const input = 'Die Spachtl'
+    await evaluate(
+      input,
+      `${labels.exercises.write.feedback.almostCorrect1} „${input}“ ${labels.exercises.write.feedback.almostCorrect2}`
+    )
+  })
+
+  it('should show wrong feedback', async () => {
+    evaluate(
+      'Das Falsche',
+      `${labels.exercises.write.feedback.wrong} ${defaultAnswerSectionProps.documents[0].article.value} ${defaultAnswerSectionProps.documents[0].word}`
+    )
   })
 })
