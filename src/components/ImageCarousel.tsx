ea2b7d0329ea00f9e16b503f9bf8a5854bfd845f
steffi <metzger@integreat-app.de>
2021-08-11 13:11:44 +0200

    LUN-130: Fix prettier

--- src/components/ImageCarousel.tsx
+++ src/components/ImageCarousel.tsx
@@ -1,59 +1,66 @@
 import React from 'react'
-import {useWindowDimensions, View} from 'react-native'
-import {ImagesType} from '../constants/endpoints'
+import { useWindowDimensions, View } from 'react-native'
+import { ImagesType } from '../constants/endpoints'
 import styled from 'styled-components/native'
-import ImageViewer from "react-native-image-zoom-viewer";
-import {COLORS} from "../constants/colors";
-import {Pagination} from "react-native-snap-carousel";
+import ImageViewer from 'react-native-image-zoom-viewer'
+import { COLORS } from '../constants/colors'
+import { Pagination } from 'react-native-snap-carousel'
 
 const StyledImage = styled.Image`
   height: 100%;
 `
 
 const PaginationView = styled.View`
-    position: absolute;
-    left: 0;
-    right: 0;
-    top: -10;
-    backgroundColor: transparent;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: -10;
+  backgroundcolor: transparent;
 `
 
 interface ImageCarouselPropsType {
-    images: ImagesType
+  images: ImagesType
 }
 
 interface ImageUrlType {
-    url: string
+  url: string
 }
 
-const ImageCarousel = ({images}: ImageCarouselPropsType) => {
-    const imagesUrls: ImageUrlType[] = []
-    images.forEach(it => imagesUrls.push({url: it.image}))
-    const {height: viewportHeight, width: viewportWidth} = useWindowDimensions()
-    console.log(viewportWidth)
+const ImageCarousel = ({ images }: ImageCarouselPropsType) => {
+  const imagesUrls: ImageUrlType[] = []
+  images.forEach(it => imagesUrls.push({ url: it.image }))
+  const { height: viewportHeight, width: viewportWidth } = useWindowDimensions()
+  console.log(viewportWidth)
 
-    const renderIndicator = (currentIndex?: number, allSize?: number) => {
-        return (
-            !currentIndex || !allSize ? <></> :
-                <PaginationView>
-                    <Pagination activeDotIndex={currentIndex - 1} dotsLength={allSize}
-                                dotStyle={{backgroundColor: COLORS.lunesBlack}}/>
-                </PaginationView>
-        )
-    }
-
-    const renderItem = (props: any) => {
-        const {source} = props
-        return <StyledImage source={source}/>
-    }
-
-    return (
-        <View style={{height: 0.4*viewportHeight, width: 1.5*viewportWidth, marginLeft: -105}}>
-            <ImageViewer imageUrls={imagesUrls} renderImage={renderItem} renderIndicator={renderIndicator}
-                         backgroundColor={COLORS.lunesWhite}/>
-        </View>
+  const renderIndicator = (currentIndex?: number, allSize?: number) => {
+    return !currentIndex || !allSize ? (
+      <></>
+    ) : (
+      <PaginationView>
+        <Pagination
+          activeDotIndex={currentIndex - 1}
+          dotsLength={allSize}
+          dotStyle={{ backgroundColor: COLORS.lunesBlack }}
+        />
+      </PaginationView>
     )
+  }
 
+  const renderItem = (props: any) => {
+    const { source } = props
+    return <StyledImage source={source} />
+  }
+
+  return (
+    <View style={{ height: 0.4 * viewportHeight, width: 1.5 * viewportWidth, marginLeft: -105 }}>
+      <ImageViewer
+        imageUrls={imagesUrls}
+        renderImage={renderItem}
+        renderIndicator={renderIndicator}
+        backgroundColor={COLORS.lunesWhite}
+      />
+    </View>
+  )
 }
 
 export default ImageCarousel
--- src/routes/vocabulary-list/components/VocabularyListItem.tsx
+++ src/routes/vocabulary-list/components/VocabularyListItem.tsx
@@ -1,115 +1,114 @@
-import React, {useState} from 'react'
-import {Image, Modal, Pressable, StyleSheet, Text, TouchableOpacity, View} from 'react-native'
-import {COLORS} from '../../../constants/colors'
-import {getArticleColor} from '../../../services/helpers'
-import {widthPercentageToDP as wp} from 'react-native-responsive-screen'
+import React, { useState } from 'react'
+import { Image, Modal, Pressable, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { COLORS } from '../../../constants/colors'
+import { getArticleColor } from '../../../services/helpers'
+import { widthPercentageToDP as wp } from 'react-native-responsive-screen'
 import AudioPlayer from '../../../components/AudioPlayer'
-import {DocumentType} from '../../../constants/endpoints'
-import ImageCarousel from "../../../components/ImageCarousel";
-import styled from "styled-components/native";
-import SingleChoiceListItem from "../../choice-exercises/components/SingleChoiceListItem";
-import {CloseButton} from "../../../../assets/images";
-import labels from "../../../constants/labels.json";
+import { DocumentType } from '../../../constants/endpoints'
+import ImageCarousel from '../../../components/ImageCarousel'
+import styled from 'styled-components/native'
+import SingleChoiceListItem from '../../choice-exercises/components/SingleChoiceListItem'
+import { CloseButton } from '../../../../assets/images'
+import labels from '../../../constants/labels.json'
 
 export const styles = StyleSheet.create({
-    wrapper: {
-        paddingHorizontal: 16
-    },
-    container: {
-        paddingVertical: 17,
-        paddingHorizontal: 16,
-        marginBottom: 8,
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        width: '100%',
-        backgroundColor: COLORS.white,
-        borderColor: COLORS.lunesBlackUltralight,
-        borderWidth: 1,
-        borderStyle: 'solid',
-        borderRadius: 2
-    },
-    item: {
-        flexDirection: 'row',
-        alignItems: 'center'
-    },
-    image: {
-        marginRight: 15,
-        width: wp('15%'),
-        height: wp('15%'),
-        borderRadius: 50
-    },
-    title: {
-        fontSize: wp('3.5%'),
-        fontWeight: 'normal',
-        borderRadius: 10,
-        marginBottom: 6,
-        color: COLORS.lunesGreyDark,
-        fontFamily: 'SourceSansPro-Regular',
-        alignSelf: 'flex-start',
-        width: wp('10%'),
-        overflow: 'hidden',
-        height: wp('5%'),
-        textAlign: 'center',
-        lineHeight: 18
-    },
-    description: {
-        fontSize: wp('4%'),
-        fontWeight: 'normal',
-        color: COLORS.lunesGreyMedium,
-        fontFamily: 'SourceSansPro-Regular',
-        marginLeft: 8
-    },
-    speaker: {
-        paddingRight: 40,
-        paddingTop: 17
-    },
-    shadow: {
-        shadowColor: COLORS.shadow,
-        shadowOffset: {width: 0, height: 5},
-        shadowRadius: 3,
-        shadowOpacity: 10
-    }
+  wrapper: {
+    paddingHorizontal: 16
+  },
+  container: {
+    paddingVertical: 17,
+    paddingHorizontal: 16,
+    marginBottom: 8,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: '100%',
+    backgroundColor: COLORS.white,
+    borderColor: COLORS.lunesBlackUltralight,
+    borderWidth: 1,
+    borderStyle: 'solid',
+    borderRadius: 2
+  },
+  item: {
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  image: {
+    marginRight: 15,
+    width: wp('15%'),
+    height: wp('15%'),
+    borderRadius: 50
+  },
+  title: {
+    fontSize: wp('3.5%'),
+    fontWeight: 'normal',
+    borderRadius: 10,
+    marginBottom: 6,
+    color: COLORS.lunesGreyDark,
+    fontFamily: 'SourceSansPro-Regular',
+    alignSelf: 'flex-start',
+    width: wp('10%'),
+    overflow: 'hidden',
+    height: wp('5%'),
+    textAlign: 'center',
+    lineHeight: 18
+  },
+  description: {
+    fontSize: wp('4%'),
+    fontWeight: 'normal',
+    color: COLORS.lunesGreyMedium,
+    fontFamily: 'SourceSansPro-Regular',
+    marginLeft: 8
+  },
+  speaker: {
+    paddingRight: 40,
+    paddingTop: 17
+  },
+  shadow: {
+    shadowColor: COLORS.shadow,
+    shadowOffset: { width: 0, height: 5 },
+    shadowRadius: 3,
+    shadowOpacity: 10
+  }
 })
 
 export interface VocabularyListItemPropType {
-    document: DocumentType
-    setIsModalVisible?: (document: DocumentType) => void
+  document: DocumentType
+  setIsModalVisible?: (document: DocumentType) => void
 }
 
-const VocabularyListItem = ({document, setIsModalVisible}: VocabularyListItemPropType) => {
-    const {article, word} = document
+const VocabularyListItem = ({ document, setIsModalVisible }: VocabularyListItemPropType) => {
+  const { article, word } = document
 
-    return (
-        <View style={styles.wrapper}>
-            <View style={styles.container}>
-                <Pressable style={styles.item} onPress={setIsModalVisible ? () => setIsModalVisible(document) : () => {}}>
-                    {document.document_image.length > 0 && (
-                        <Image
-                            source={{
-                                uri: document.document_image[0].image
-                            }}
-                            width={24}
-                            height={24}
-                            style={styles.image}
-                        />
-                    )}
-                    <View>
-                        <Text testID='article' style={[styles.title, {backgroundColor: getArticleColor(article)}]}>
-                            {article.value}
-                        </Text>
-                        <Text testID='word' style={styles.description}>
-                            {word}
-                        </Text>
-                    </View>
-                </Pressable>
-                <View style={styles.speaker}>
-                    <AudioPlayer document={document} disabled={false}/>
-                </View>
-            </View>
-
+  return (
+    <View style={styles.wrapper}>
+      <View style={styles.container}>
+        <Pressable style={styles.item} onPress={setIsModalVisible ? () => setIsModalVisible(document) : () => {}}>
+          {document.document_image.length > 0 && (
+            <Image
+              source={{
+                uri: document.document_image[0].image
+              }}
+              width={24}
+              height={24}
+              style={styles.image}
+            />
+          )}
+          <View>
+            <Text testID='article' style={[styles.title, { backgroundColor: getArticleColor(article) }]}>
+              {article.value}
+            </Text>
+            <Text testID='word' style={styles.description}>
+              {word}
+            </Text>
+          </View>
+        </Pressable>
+        <View style={styles.speaker}>
+          <AudioPlayer document={document} disabled={false} />
         </View>
-    )
+      </View>
+    </View>
+  )
 }
 
 export default VocabularyListItem
