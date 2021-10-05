b02d8c818180e44fa29e2c436b3ba07864e8f670
Sarah Sporck <sporck@integreat-app.de>
2021-10-05 10:54:40 +0200

    LUN-147: add additional typography to fonts

--- src/constants/theme/fonts.ts
+++ src/constants/theme/fonts.ts
@@ -5,6 +5,10 @@ export const FONTS = {
   contentFontRegular: 'SourceSansPro-Regular',
   defaultFontWeight: 600,
   lightFontWeight: 400,
+  capsLetterSpacing: '0.4px',
+  listTitleLetterSpacing: '0.11px',
+  smallFontSize: `${wp('3%')}px`,
   defaultFontSize: `${wp('4%')}px`,
-  largeFontSize: `${wp('5%')}px`
+  largeFontSize: `${wp('4.5%')}px`,
+  headingFontSize: `${wp('5%')}px`
 }
