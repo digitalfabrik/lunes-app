48105b93bdf0d6841dca8878bf79962e7ccbedb7
steffi <metzger@integreat-app.de>
2021-10-07 10:19:28 +0200

    LUN-69: Optimized prefix

--- release-notes/createReleaseNotes.sh
+++ release-notes/createReleaseNotes.sh
@@ -1,8 +1,7 @@
 branch=`git rev-parse --abbrev-ref HEAD`
 file=release-notes/unreleased/$branch.yml
 echo $branch
-prefix=${branch:0:7}
 touch $file
-echo issue_key: $prefix >> $file
+echo issue_key: `expr $branch : '^\(LUN-[0-9]*\)'` >> $file
 out="show_in_stores: true\nplatforms:\n  - android\n  - ios\nde: $1"
 echo -e "$out" >> $file
