df3d5748576ac6cb29a9e2006e4b5b79154e760e
steffi <metzger@integreat-app.de>
2021-10-12 13:47:06 +0200

    LUN-127: Added test

--- src/hocs/withCustomDiscipline.tsx
+++ src/hocs/withCustomDiscipline.tsx
@@ -6,22 +6,18 @@ import AsyncStorage from '../services/AsyncStorage'
 const withCustomDiscipline = <Props extends { customDisciplines: string[] }>(
   Component: React.ComponentType<Props>
 ): React.ComponentType<Omit<Props, 'customDisciplines'>> => {
-  const Wrapper = (props: any) => {
+  return (props: any) => {
     const [customDisciplines, setCustomDisciplines] = useState<string[]>([])
     const isFocused = useIsFocused()
 
     useEffect(() => {
       AsyncStorage.getCustomDisciplines()
-        .then((res: string[]) => {
-          res === null ? setCustomDisciplines([]) : setCustomDisciplines(res)
-        })
+        .then((res: string[]) => (res === null ? setCustomDisciplines([]) : setCustomDisciplines(res)))
         .catch((err: Error) => console.log('Error while fetching custom disciplines ', err))
     }, [isFocused])
 
     return <Component customDisciplines={customDisciplines} {...props} />
   }
-
-  return Wrapper
 }
 
 export default withCustomDiscipline
