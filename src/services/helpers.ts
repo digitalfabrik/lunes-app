f918dd9f9afcde47119efe193e3c56c03ec2a7a4
Raphael Hepperle <raphael.hepp.rh@gmail.com>
2021-10-14 14:16:11 +0200

    LUN-121: add shuffleArray function

--- src/services/helpers.ts
+++ src/services/helpers.ts
@@ -19,3 +19,15 @@ export const getArticleColor = (article: Article): string => {
       return COLORS.lunesArtikelDer
   }
 }
+
+/* Randomize array using Durstenfeld shuffle algorithm with O(n) complexity */
+export const shuffleArray = <T>(array: T[]): T[] => {
+  array = array.slice()
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1))
+    const temp = array[i]
+    array[i] = array[j]
+    array[j] = temp
+  }
+  return array
+}
