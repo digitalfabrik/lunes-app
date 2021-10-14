89387b4021e0f3beeebde4c986e5599c6f8ec5db
Raphael Hepperle <raphael.hepp.rh@gmail.com>
2021-10-14 14:45:55 +0200

    LUN-121: add useLoadDocumentsRandomOrder custom hook

--- src/hooks/useLoadDocuments.ts
+++ src/hooks/useLoadDocuments.ts
@@ -1,6 +1,7 @@
 import { ARTICLES } from '../constants/data'
 import { DocumentType, ENDPOINTS } from '../constants/endpoints'
 import useLoadFromEndpoint, { ReturnType } from './useLoadFromEndpoint'
+import { shuffleArray } from "../services/helpers";
 
 export interface AlternativeWordTypeFromServer {
   article: number
@@ -33,6 +34,15 @@ const useLoadDocuments = (trainingSetId: number): ReturnType<DocumentType[]> => 
   const url = ENDPOINTS.documents.replace(':id', `${trainingSetId}`)
   const documents: ReturnType<DocumentTypeFromServer[]> = useLoadFromEndpoint(url)
   return transformArticle(documents)
+}
+
+export const useLoadDocumentsRandomOrder = (trainingSetId: number): ReturnType<DocumentType[]> => {
+  const documents: ReturnType<DocumentType[]> = useLoadDocuments(trainingSetId)
+  if (!documents.data) {
+    return documents
+  }
+  const shuffledDocuments = shuffleArray(documents.data)
+  return { ...documents, data: shuffledDocuments }
 }
 
 export default useLoadDocuments
