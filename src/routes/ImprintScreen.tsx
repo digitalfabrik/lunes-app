9b73d45e028c4f228613a8cc9b0ff9f481228c57
Sarah Sporck <sporck@integreat-app.de>
2022-03-02 13:51:19 +0100

    LUN-122: move padding to root container

--- src/routes/ImprintScreen.tsx
+++ src/routes/ImprintScreen.tsx
@@ -2,7 +2,7 @@ import React, { ReactElement } from 'react'
 import styled from 'styled-components/native'
 
 import Link from '../components/Link'
-import { ContentTextLight } from '../components/text/Content'
+import { ContentText } from '../components/text/Content'
 import { HeadingText } from '../components/text/Heading'
 import { Subheading } from '../components/text/Subheading'
 
@@ -10,13 +10,10 @@ const Root = styled.View`
   background-color: ${prop => prop.theme.colors.background};
   height: 100%;
   width: 100%;
+  padding: ${props => props.theme.spacings.sm};
 `
 
-const Container = styled.View`
-  margin: ${props => props.theme.spacings.sm};
-`
-
-const Description = styled(ContentTextLight)`
+const Description = styled(ContentText)`
   margin-bottom: ${props => props.theme.spacings.xs};
 `
 
@@ -30,29 +27,27 @@ const MultiTextContainer = styled.View`
 
 const ImprintScreen = (): ReactElement => (
   <Root>
-    <Container>
-      <Title>Impressum</Title>
-      <Description>Angaben gemäß § 5 TMG und verantwortlich für den Inhalt nach § 55 Abs. 2 RStV:</Description>
-      <Description>Tür an Tür – Digitalfabrik gGmbH Wertachstr. 29 86153 Augsburg</Description>
-      <Description>vertreten durch Herrn Daniel Kehne, Herrn Fritjof Knier</Description>
-      <Subheading>Aufsichtsbehörde:</Subheading>
-      <Description>Finanzamt Augsburg-Stadt</Description>
-      <Subheading>Sitz der Gesellschaft:</Subheading>
-      <Description>Augsburg</Description>
-      <Subheading>Handelsregister und Registernummer:</Subheading>
-      <Description>HRB30759</Description>
-      <Subheading>Umsatzsteuer-Identifikationsnummer:</Subheading>
-      <Description>DE307491397</Description>
-      <Subheading>Kontakt</Subheading>
-      <MultiTextContainer>
-        <Description>Telefon: </Description>
-        <Link text='0821 20990556' url='tel:082120990556' />
-      </MultiTextContainer>
-      <MultiTextContainer>
-        <Description>E-Mail: </Description>
-        <Link text='digitalfabrik@tuerantuer.de' url='mailto:digitalfabrik@tuerantuer.de' />
-      </MultiTextContainer>
-    </Container>
+    <Title>Impressum</Title>
+    <Description>Angaben gemäß § 5 TMG und verantwortlich für den Inhalt nach § 55 Abs. 2 RStV:</Description>
+    <Description>Tür an Tür – Digitalfabrik gGmbH Wertachstr. 29 86153 Augsburg</Description>
+    <Description>vertreten durch Herrn Daniel Kehne, Herrn Fritjof Knier</Description>
+    <Subheading>Aufsichtsbehörde:</Subheading>
+    <Description>Finanzamt Augsburg-Stadt</Description>
+    <Subheading>Sitz der Gesellschaft:</Subheading>
+    <Description>Augsburg</Description>
+    <Subheading>Handelsregister und Registernummer:</Subheading>
+    <Description>HRB30759</Description>
+    <Subheading>Umsatzsteuer-Identifikationsnummer:</Subheading>
+    <Description>DE307491397</Description>
+    <Subheading>Kontakt</Subheading>
+    <MultiTextContainer>
+      <ContentText>Telefon: </ContentText>
+      <Link text='0821 20990556' url='tel:082120990556' />
+    </MultiTextContainer>
+    <MultiTextContainer>
+      <ContentText>E-Mail: </ContentText>
+      <Link text='digitalfabrik@tuerantuer.de' url='mailto:digitalfabrik@tuerantuer.de' />
+    </MultiTextContainer>
   </Root>
 )
 
