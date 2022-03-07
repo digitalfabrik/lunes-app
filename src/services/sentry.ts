27f31ced16b23a553f0527874281046563bdf9d7
steffi <metzger@integreat-app.de>
2022-03-07 14:22:20 +0100

    LUN-33: Setup Sentry

---
+++ src/services/sentry.ts
@@ -0,0 +1,47 @@
+/* eslint-disable no-console */
+import * as Sentry from '@sentry/react-native'
+
+export const initSentry = (): void => {
+  if (!__DEV__) {
+    Sentry.init({
+      dsn: 'https://8e99e20eb419461d8a539a15e09199d8@sentry.tuerantuer.org/3'
+    })
+  }
+}
+
+export const log = (message: string, level = 'debug'): void => {
+  if (__DEV__) {
+    switch (level) {
+      case Sentry.Severity.Fatal:
+      case Sentry.Severity.Critical:
+      case Sentry.Severity.Error:
+        console.error(message)
+        break
+      case Sentry.Severity.Warning:
+        console.warn(message)
+        break
+      case Sentry.Severity.Log:
+        console.log(message)
+        break
+      case Sentry.Severity.Info:
+        console.info(message)
+        break
+      case Sentry.Severity.Debug:
+        console.debug(message)
+        break
+    }
+  } else {
+    Sentry.addBreadcrumb({
+      message,
+      level: Sentry.Severity.fromString(level)
+    })
+  }
+}
+
+export const reportError = (err: unknown): void => {
+  if (__DEV__) {
+    console.error('Sentry reported error: ', err)
+  } else {
+    Sentry.captureException(err)
+  }
+}
