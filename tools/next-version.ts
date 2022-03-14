7eab77fc72c54a1612d3c8951a83c4836bdca073
steffi <metzger@integreat-app.de>
2022-03-14 14:55:07 +0100

    LUN-30: Change tools folder to ts

--- tools/next-version.ts
+++ tools/next-version.ts
@@ -1,31 +1,28 @@
-#!/usr/bin/env node
+/* eslint-disable no-console */
+import { program } from 'commander'
+import fs from 'fs'
+import path from 'path'
 
 import { VERSION_FILE } from './constants'
 
-const fs = require('fs')
-const { program } = require('commander')
-
 const calculateNewVersion = () => {
-  const versionFile = fs.readFileSync(VERSION_FILE)
+  const versionFile = fs.readFileSync(path.resolve(__dirname, '..', VERSION_FILE), 'utf-8')
+  // versionCode is just used in the integreat-react-native-app
   const { versionName, versionCode } = JSON.parse(versionFile)
-  const versionNameParts = versionName.split('.').map(it => parseInt(it))
+  const versionNameParts = versionName.split('.').map((it: string) => parseInt(it, 10))
 
   const date = new Date()
   const year = date.getFullYear()
   const month = date.getMonth() + 1
 
-  const versionNameCounter = year === versionNameParts[0] && month === versionNameParts[1] ? versionNameParts[2] + 1 : 0
+  const versionNameCounter: number =
+    year === versionNameParts[0] && month === versionNameParts[1] ? versionNameParts[2] + 1 : 0
   const newVersionName = `${year}.${month}.${versionNameCounter}`
 
   if (versionCode && typeof versionCode !== 'number') {
     throw new Error(`Version code must be a number, but is of type ${typeof versionCode}.`)
   }
-  const newVersionCode = versionCode ? versionCode + 1 : undefined
-
-  console.warn(`New version name: ${newVersionName}`)
-  if (newVersionCode) {
-    console.warn(`New version name: ${newVersionCode}`)
-  }
+  const newVersionCode: number = versionCode ? versionCode + 1 : undefined
 
   return {
     versionName: newVersionName,
