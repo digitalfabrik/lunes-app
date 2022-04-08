247c28e046cd69a9e0ac666086a64f12881347eb
Andy <fischer@integreat-app.de>
2022-04-08 10:57:36 +0200

    LUN-282: use sentry log fkt

--- src/services/url.ts
+++ src/services/url.ts
@@ -1,5 +1,7 @@
 import { Linking } from 'react-native'
 
+import { log } from './sentry'
+
 // fix ios issue for Django, that requires trailing slash in request url https://github.com/square/retrofit/issues/1037
 export const addTrailingSlashToUrl = (url: string): string => (url.endsWith('/') ? url : `${url}/`)
 
@@ -8,7 +10,7 @@ export const openExternalUrl = (url: string): Promise<void> =>
     .then(supported => {
       if (!supported) {
         // iOS Simulators do not support certain schemes like mailto or tel
-        console.log('Cannot handle url', url)
+        log(`Cannot handle url: ${url}`)
         return null
       }
       return Linking.openURL(url)
