559d1ec39eb9adf2beacd503bf9ef20ac1d67d6d
Steffen Kleinle <steffen.kleinle@mailbox.org>
2022-05-18 11:55:41 +0200

    LUN-265: Add bottom tabs dependency and upgrade reactnavigation

--- src/testing/createNavigationPropMock.ts
+++ src/testing/createNavigationPropMock.ts
@@ -25,7 +25,8 @@ const createNavigationMock = <T extends keyof RoutesParams>(routeIndex = 0): Sta
   isFocused: jest.fn(),
   reset: jest.fn(),
   removeListener: jest.fn(),
-  setOptions: jest.fn()
+  setOptions: jest.fn(),
+  getId: jest.fn()
 })
 
 export default createNavigationMock
