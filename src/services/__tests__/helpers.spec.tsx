f04583382189abb9b5711e1c582ec5afab3c2a74
Sarah Sporck <sporck@integreat-app.de>
2022-05-25 16:07:56 +0200

    LUN-317: Remove unneccessary beforeEach and update browserlist

--- src/services/__tests__/helpers.spec.tsx
+++ src/services/__tests__/helpers.spec.tsx
@@ -10,10 +10,6 @@ jest.mock('../../hooks/useLoadDisciplines')
 jest.mock('../AsyncStorage')
 
 describe('helpers', () => {
-  beforeEach(() => {
-    jest.clearAllMocks()
-  })
-
   const profession = {
     id: 10,
     title: 'Parent Discipline',
