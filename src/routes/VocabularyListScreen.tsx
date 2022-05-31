2aa4801f4312c3a72fa6a8e5c6618e9be7b04e8f
Steffen Kleinle <steffen.kleinle@mailbox.org>
2022-05-31 12:49:19 +0200

    LUN-357: Readd saving of progress and tests

--- src/routes/VocabularyListScreen.tsx
+++ src/routes/VocabularyListScreen.tsx
@@ -1,9 +1,12 @@
 import { RouteProp } from '@react-navigation/native'
 import { StackNavigationProp } from '@react-navigation/stack'
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import VocabularyList from '../components/VocabularyList'
+import { ExerciseKeys } from '../constants/data'
 import { RoutesParams } from '../navigation/NavigationTypes'
+import AsyncStorage from '../services/AsyncStorage'
+import { reportError } from '../services/sentry'
 
 interface VocabularyListScreenProps {
   route: RouteProp<RoutesParams, 'VocabularyList'>
@@ -11,8 +14,15 @@ interface VocabularyListScreenProps {
 }
 
 const VocabularyListScreen = ({ route, navigation }: VocabularyListScreenProps): JSX.Element => {
+  const { disciplineId } = route.params
+
+  useEffect(() => {
+    AsyncStorage.setExerciseProgress(disciplineId, ExerciseKeys.vocabularyList, 1).catch(reportError)
+  }, [disciplineId])
+
   const onItemPress = (index: number) =>
     navigation.navigate('VocabularyDetail', { ...route.params, documentIndex: index })
+
   return <VocabularyList documents={route.params.documents} onItemPress={onItemPress} />
 }
 
