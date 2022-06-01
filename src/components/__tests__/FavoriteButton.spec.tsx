6c7e40f73130b1c4c8e4339247a120260aa0088f
Steffen Kleinle <steffen.kleinle@mailbox.org>
2022-06-01 15:58:15 +0200

    LUN-357: Fix tests

--- src/components/__tests__/FavoriteButton.spec.tsx
+++ src/components/__tests__/FavoriteButton.spec.tsx
@@ -1,3 +1,4 @@
+import { NavigationContainer } from '@react-navigation/native'
 import { fireEvent, waitFor } from '@testing-library/react-native'
 import React from 'react'
 
@@ -10,29 +11,36 @@ import FavoriteButton from '../FavoriteButton'
 describe('FavoriteButton', () => {
   const document = new DocumentBuilder(1).build()[0]
 
+  const renderFavoriteButton = () =>
+    render(
+      <NavigationContainer>
+        <FavoriteButton document={document} />
+      </NavigationContainer>
+    )
+
   it('should add favorite on click', async () => {
     await AsyncStorage.setFavorites([])
     await expect(AsyncStorage.isFavorite(document)).resolves.toBe(false)
 
-    const { getByA11yLabel } = render(<FavoriteButton document={document} />)
+    const { getByA11yLabel } = renderFavoriteButton()
 
     await waitFor(() => expect(getByA11yLabel(labels.favorites.add)).toBeTruthy())
     fireEvent.press(getByA11yLabel(labels.favorites.add))
 
-    expect(getByA11yLabel(labels.favorites.remove)).toBeTruthy()
-    await waitFor(() => expect(AsyncStorage.isFavorite(document)).resolves.toBe(true))
+    await waitFor(() => expect(getByA11yLabel(labels.favorites.remove)).toBeTruthy())
+    await expect(AsyncStorage.isFavorite(document)).resolves.toBe(true)
   })
 
   it('should remove favorite on click', async () => {
     await AsyncStorage.setFavorites([document])
     await expect(AsyncStorage.isFavorite(document)).resolves.toBe(true)
 
-    const { getByA11yLabel } = render(<FavoriteButton document={document} />)
+    const { getByA11yLabel } = renderFavoriteButton()
 
     await waitFor(() => expect(getByA11yLabel(labels.favorites.remove)).toBeTruthy())
     fireEvent.press(getByA11yLabel(labels.favorites.remove))
 
-    expect(getByA11yLabel(labels.favorites.add)).toBeTruthy()
-    await waitFor(() => expect(AsyncStorage.isFavorite(document)).resolves.toBe(false))
+    await waitFor(() => expect(getByA11yLabel(labels.favorites.add)).toBeTruthy())
+    await expect(AsyncStorage.isFavorite(document)).resolves.toBe(false)
   })
 })
