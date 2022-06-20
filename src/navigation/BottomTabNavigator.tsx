4955040958ba7ea3e2a34fa3eda53bada817914f
Andy <fischer@integreat-app.de>
2022-06-20 20:46:48 +0200

    LUN-365: fix padding for safe area view on ios

--- src/navigation/BottomTabNavigator.tsx
+++ src/navigation/BottomTabNavigator.tsx
@@ -1,7 +1,7 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import React from 'react'
-import { SafeAreaView } from 'react-native'
 import { widthPercentageToDP as wp } from 'react-native-responsive-screen'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import { useTheme } from 'styled-components/native'
 
 import { HomeIconGrey, HomeIconWhite } from '../../assets/images'
@@ -13,78 +13,78 @@ const Navigator = createBottomTabNavigator<RoutesParams>()
 
 const BottomTabNavigator = (): JSX.Element | null => {
   const theme = useTheme()
+  const insets = useSafeAreaInsets()
 
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <Navigator.Navigator
-        screenOptions={{
-          headerShown: false,
-          tabBarActiveTintColor: theme.colors.background,
-          tabBarStyle: {
-            backgroundColor: theme.colors.primary,
-            height: wp('14%'),
-            // TODO LUN-132: Delete
-            display: 'none'
-          },
-          tabBarItemStyle: { height: wp('14%'), padding: wp('2%') },
-          tabBarLabelStyle: { fontSize: wp('3%') }
-        }}>
-        <Navigator.Screen
-          name='HomeTab'
-          component={HomeStackNavigator}
-          options={{
-            tabBarIcon: ({ focused }) =>
-              focused ? (
-                <HomeIconWhite width={wp('10%')} height={wp('10%')} />
-              ) : (
-                <HomeIconGrey width={wp('10%')} height={wp('10%')} />
-              ),
-            title: labels.general.home
-          }}
-        />
-        {/* TODO LUN-132: Uncomment */}
-        {/* <Navigator.Screen */}
-        {/*  name='FavoritesTab' */}
-        {/*  component={MockComponent} */}
-        {/*  options={{ */}
-        {/*    tabBarIcon: ({ focused }) => */}
-        {/*      focused ? ( */}
-        {/*        <StartIconWhite width={wp('7%')} height={wp('7%')} /> */}
-        {/*      ) : ( */}
-        {/*        <StartIconGrey width={wp('7%')} height={wp('7%')} /> */}
-        {/*      ), */}
-        {/*    title: labels.general.favorites */}
-        {/*  }} */}
-        {/* /> */}
-        {/* <Navigator.Screen */}
-        {/*  name='DictionaryTab' */}
-        {/*  component={MockComponent} */}
-        {/*  options={{ */}
-        {/*    tabBarIcon: ({ focused }) => */}
-        {/*      focused ? ( */}
-        {/*        <BookIconWhite width={wp('7%')} height={wp('7%')} /> */}
-        {/*      ) : ( */}
-        {/*        <BookIconGrey width={wp('7%')} height={wp('7%')} /> */}
-        {/*      ), */}
-        {/*    title: labels.general.dictionary */}
-        {/*  }} */}
-        {/* /> */}
-        {/* TODO LUN-308: Uncomment */}
-        {/* <Navigator.Screen */}
-        {/*  name='UserVocabularyTab' */}
-        {/*  component={MockComponent} */}
-        {/*  options={{ */}
-        {/*    tabBarIcon: ({ focused }) => */}
-        {/*      focused ? ( */}
-        {/*        <HeartIconWhite width={wp('6%')} height={wp('6%')} /> */}
-        {/*      ) : ( */}
-        {/*        <HeartIconGrey width={wp('6%')} height={wp('6%')} /> */}
-        {/*      ), */}
-        {/*    title: labels.general.userVocabulary */}
-        {/*  }} */}
-        {/* /> */}
-      </Navigator.Navigator>
-    </SafeAreaView>
+    <Navigator.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: theme.colors.background,
+        tabBarStyle: {
+          backgroundColor: theme.colors.primary,
+          minHeight: wp('14%'),
+          paddingBottom: insets.bottom,
+          // TODO LUN-132: Delete
+          display: 'none'
+        },
+        tabBarItemStyle: { height: wp('14%'), padding: wp('2%') },
+        tabBarLabelStyle: { fontSize: wp('3%') }
+      }}>
+      <Navigator.Screen
+        name='HomeTab'
+        component={HomeStackNavigator}
+        options={{
+          tabBarIcon: ({ focused }) =>
+            focused ? (
+              <HomeIconWhite width={wp('10%')} height={wp('10%')} />
+            ) : (
+              <HomeIconGrey width={wp('10%')} height={wp('10%')} />
+            ),
+          title: labels.general.home
+        }}
+      />
+      {/* TODO LUN-132: Uncomment */}
+      {/* <Navigator.Screen */}
+      {/*  name='FavoritesTab' */}
+      {/*  component={MockComponent} */}
+      {/*  options={{ */}
+      {/*    tabBarIcon: ({ focused }) => */}
+      {/*      focused ? ( */}
+      {/*        <StartIconWhite width={wp('7%')} height={wp('7%')} /> */}
+      {/*      ) : ( */}
+      {/*        <StartIconGrey width={wp('7%')} height={wp('7%')} /> */}
+      {/*      ), */}
+      {/*    title: labels.general.favorites */}
+      {/*  }} */}
+      {/* /> */}
+      {/* <Navigator.Screen */}
+      {/*  name='DictionaryTab' */}
+      {/*  component={MockComponent} */}
+      {/*  options={{ */}
+      {/*    tabBarIcon: ({ focused }) => */}
+      {/*      focused ? ( */}
+      {/*        <BookIconWhite width={wp('7%')} height={wp('7%')} /> */}
+      {/*      ) : ( */}
+      {/*        <BookIconGrey width={wp('7%')} height={wp('7%')} /> */}
+      {/*      ), */}
+      {/*    title: labels.general.dictionary */}
+      {/*  }} */}
+      {/* /> */}
+      {/* TODO LUN-308: Uncomment */}
+      {/* <Navigator.Screen */}
+      {/*  name='UserVocabularyTab' */}
+      {/*  component={MockComponent} */}
+      {/*  options={{ */}
+      {/*    tabBarIcon: ({ focused }) => */}
+      {/*      focused ? ( */}
+      {/*        <HeartIconWhite width={wp('6%')} height={wp('6%')} /> */}
+      {/*      ) : ( */}
+      {/*        <HeartIconGrey width={wp('6%')} height={wp('6%')} /> */}
+      {/*      ), */}
+      {/*    title: labels.general.userVocabulary */}
+      {/*  }} */}
+      {/* /> */}
+    </Navigator.Navigator>
   )
 }
 
