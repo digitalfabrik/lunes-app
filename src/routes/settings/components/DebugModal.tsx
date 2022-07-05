779b4edc5c2fb21c1c474e5e44be5874710812a3
steffi <metzger@integreat-app.de>
2022-07-05 14:07:35 +0200

    LUN-292: Add switch for cms

--- src/routes/settings/components/DebugModal.tsx
+++ src/routes/settings/components/DebugModal.tsx
@@ -4,9 +4,18 @@ import styled from 'styled-components/native'
 
 import Button from '../../../components/Button'
 import ModalSkeleton from '../../../components/ModalSkeleton'
+import { ContentSecondary } from '../../../components/text/Content'
+import { Subheading } from '../../../components/text/Subheading'
 import { BUTTONS_THEME } from '../../../constants/data'
 import labels from '../../../constants/labels.json'
+import AsyncStorage from '../../../services/AsyncStorage'
+import { getBaseURL, liveCMS, testCMS } from '../../../services/axios'
 import { reportError } from '../../../services/sentry'
+
+const Container = styled.View`
+  align-items: center;
+  padding: ${props => props.theme.spacings.md} ${props => props.theme.spacings.xl};
+`
 
 const CodeInput = styled.TextInput`
   border: 1px solid ${props => props.theme.colors.overlay};
@@ -22,7 +31,10 @@ interface PropsType {
 const DebugModal = (props: PropsType): JSX.Element => {
   const { visible, onClose } = props
   const [inputText, setInputText] = useState<string>('')
+  const [baseURL, setBaseURL] = useState<string>('')
   const UNLOCKING_TEXT = 'wirschaffendas'
+
+  getBaseURL().then(setBaseURL).catch(reportError)
 
   const throwSentryError = (): void => {
     reportError('Error for testing Sentry')
@@ -34,15 +46,35 @@ const DebugModal = (props: PropsType): JSX.Element => {
     onClose()
   }
 
+  const switchCMS = async (): Promise<void> => {
+    const overwrite = await AsyncStorage.getOverwriteCMS()
+    const currentCMS = await getBaseURL()
+    if (overwrite) {
+      await AsyncStorage.setOverwriteCMS(overwrite === liveCMS ? testCMS : liveCMS)
+    } else {
+      await AsyncStorage.setOverwriteCMS(currentCMS === liveCMS ? testCMS : liveCMS)
+    }
+    getBaseURL().then(setBaseURL).catch(reportError)
+  }
+
   return (
     <ModalSkeleton testID='debug-modal' visible={visible} onClose={resetTextAndClose}>
       <CodeInput placeholder='Development Code' onChangeText={setInputText} />
       {inputText.toLowerCase() === UNLOCKING_TEXT && (
-        <Button
-          label={labels.settings.debugModal.sentry}
-          onPress={throwSentryError}
-          buttonTheme={BUTTONS_THEME.contained}
-        />
+        <Container>
+          <Button
+            label={labels.settings.debugModal.sentry}
+            onPress={throwSentryError}
+            buttonTheme={BUTTONS_THEME.contained}
+          />
+          <Subheading>{labels.settings.debugModal.currentCMS}:</Subheading>
+          <ContentSecondary>{baseURL}</ContentSecondary>
+          <Button
+            label={labels.settings.debugModal.changeCMS}
+            onPress={switchCMS}
+            buttonTheme={BUTTONS_THEME.contained}
+          />
+        </Container>
       )}
     </ModalSkeleton>
   )
--- src/routes/settings/components/__tests__/DebugModal.spec.tsx
+++ src/routes/settings/components/__tests__/DebugModal.spec.tsx
@@ -1,7 +1,8 @@
-import { fireEvent } from '@testing-library/react-native'
+import { act, fireEvent } from '@testing-library/react-native'
 import React from 'react'
 
 import labels from '../../../../constants/labels.json'
+import { liveCMS, testCMS } from '../../../../services/axios'
 import render from '../../../../testing/render'
 import DebugModal from '../DebugModal'
 
@@ -12,5 +13,24 @@ describe('DebugModal', () => {
     const textField = getByPlaceholderText('Development Code')
     fireEvent.changeText(textField, 'wirschaffendas')
     expect(queryByText(labels.settings.debugModal.sentry)).not.toBeNull()
+  })
+
+  it('should show and switch cms url', async () => {
+    const { getByText, getByPlaceholderText } = render(<DebugModal visible onClose={jest.fn()} />)
+    const textField = getByPlaceholderText('Development Code')
+    await act(async () => {
+      await fireEvent.changeText(textField, 'wirschaffendas')
+    })
+    expect(getByText(testCMS)).toBeDefined()
+    const switchCMSButton = getByText(labels.settings.debugModal.changeCMS)
+    expect(switchCMSButton).toBeDefined()
+    await act(async () => {
+      await fireEvent.press(switchCMSButton)
+    })
+    expect(getByText(liveCMS)).toBeDefined()
+    await act(async () => {
+      await fireEvent.press(switchCMSButton)
+    })
+    expect(getByText(testCMS)).toBeDefined()
   })
 })
