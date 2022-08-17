e4f3607a2c8670355b6a7abb5292cedabc0c362b
steffi <metzger@integreat-app.de>
2022-08-17 08:55:07 +0200

    LUN-394: Small adjustments

--- src/routes/UserVocabularyOverviewScreen.tsx
+++ src/routes/UserVocabularyOverviewScreen.tsx
@@ -4,22 +4,22 @@ import styled from 'styled-components/native'
 
 import { BookIconBlack } from '../../assets/images'
 import ListItem from '../components/ListItem'
+import RouteWrapper from '../components/RouteWrapper'
 import { Heading } from '../components/text/Heading'
-import theme from '../constants/theme'
 import { RoutesParams } from '../navigation/NavigationTypes'
 import { getLabels } from '../services/helpers'
 
 const Root = styled.View`
-  padding: ${theme.spacings.md};
+  padding: ${props => props.theme.spacings.md};
 `
 
 const StyledHeading = styled(Heading)`
   text-align: center;
-  margin: ${theme.spacings.sm};
+  margin: ${props => props.theme.spacings.sm};
 `
 
 const Margin = styled.View`
-  margin: ${theme.spacings.xl};
+  margin: ${props => props.theme.spacings.xl};
 `
 
 interface PropsType {
@@ -27,25 +27,27 @@ interface PropsType {
 }
 
 const UserVocabularyOverviewScreen = ({ navigation }: PropsType): JSX.Element => (
-  <Root>
-    <StyledHeading>{getLabels().ownVocabulary.myVocabulary}</StyledHeading>
-    <Margin />
-    <ListItem
-      icon={<BookIconBlack />}
-      title={getLabels().ownVocabulary.overview.list}
-      onPress={() => navigation.navigate('UserVocabularyOverview')}
-    />
-    <ListItem
-      icon={<BookIconBlack />}
-      title={getLabels().ownVocabulary.overview.create}
-      onPress={() => navigation.navigate('UserVocabularyOverview')}
-    />
-    <ListItem
-      icon={<BookIconBlack />}
-      title={getLabels().ownVocabulary.overview.practice}
-      onPress={() => navigation.navigate('UserVocabularyOverview')}
-    />
-  </Root>
+  <RouteWrapper>
+    <Root>
+      <StyledHeading>{getLabels().ownVocabulary.myVocabulary}</StyledHeading>
+      <Margin />
+      <ListItem
+        icon={<BookIconBlack />}
+        title={getLabels().ownVocabulary.overview.list}
+        onPress={() => navigation.navigate('UserVocabularyOverview')}
+      />
+      <ListItem
+        icon={<BookIconBlack />}
+        title={getLabels().ownVocabulary.overview.create}
+        onPress={() => navigation.navigate('UserVocabularyOverview')}
+      />
+      <ListItem
+        icon={<BookIconBlack />}
+        title={getLabels().ownVocabulary.overview.practice}
+        onPress={() => navigation.navigate('UserVocabularyOverview')}
+      />
+    </Root>
+  </RouteWrapper>
 )
 
 export default UserVocabularyOverviewScreen
