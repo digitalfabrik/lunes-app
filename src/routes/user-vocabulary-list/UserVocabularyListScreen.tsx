a672feb49a04a6f8aaaa4a64f14e42294421f137
Andy <fischer@integreat-app.de>
2022-09-21 15:51:47 +0200

    LUN-396: fix labels on UserVocabularyListScreen

--- src/routes/user-vocabulary-list/UserVocabularyListScreen.tsx
+++ src/routes/user-vocabulary-list/UserVocabularyListScreen.tsx
@@ -50,8 +50,16 @@ const UserVocabularyListScreen = ({ navigation }: Props): ReactElement => {
 
   const numberOfDocuments = documents.data?.length ?? 0
   const sortedAndFilteredDocuments = getSortedAndFilteredDocuments(documents.data, searchString)
-  const { create, myWords, list } = getLabels().userVocabulary
-  const { confirm, confirmDeletionPart1, confirmDeletionPart2, finished, edit } = list
+  const { create, myWords } = getLabels().userVocabulary
+  const { list } = getLabels().userVocabulary.overview
+  const {
+    confirm,
+    confirmDeletionPart1,
+    confirmDeletionPart2,
+    finished,
+    edit,
+  }: { confirm: string; confirmDeletionPart1: string; confirmDeletionPart2: string; finished: string; edit: string } =
+    getLabels().userVocabulary.list
 
   // AsyncStorage.addUserDocument(new DocumentBuilder(4).build()[3]) /* TODO remove im LUN-401 */
 
