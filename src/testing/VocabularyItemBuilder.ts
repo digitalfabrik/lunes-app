067f81abfdc7d08da65ed4285eccc0a7e2b54bdd
bob606 <saliu.bah654@gmail.com>
2022-09-26 17:21:01 +0200

    fix: fixed tests

---
+++ src/testing/VocabularyItemBuilder.ts
@@ -0,0 +1,64 @@
+import { ARTICLES } from '../constants/data'
+import { VocabularyItem } from '../constants/endpoints'
+
+const vocabularyItems: VocabularyItem[] = [
+  {
+    id: 1,
+    word: 'Spachtel',
+    article: ARTICLES[1],
+    document_image: [{ id: 1, image: 'Spachtel' }],
+    audio: 'https://example.com/my-audio',
+    alternatives: [
+      {
+        word: 'Spachtel',
+        article: ARTICLES[2],
+      },
+      {
+        word: 'Alternative',
+        article: ARTICLES[2],
+      },
+    ],
+  },
+  {
+    id: 2,
+    word: 'Auto',
+    article: ARTICLES[3],
+    document_image: [{ id: 1, image: 'Auto' }],
+    audio: '',
+    alternatives: [],
+  },
+  {
+    audio: '',
+    word: 'Hose',
+    id: 3,
+    article: ARTICLES[2],
+    document_image: [{ id: 1, image: 'image' }],
+    alternatives: [],
+  },
+  {
+    audio: '',
+    word: 'Helm',
+    id: 4,
+    article: ARTICLES[1],
+    document_image: [{ id: 2, image: 'image' }],
+    alternatives: [],
+  },
+]
+
+class VocabularyItemBuilder {
+  documentCount: number
+
+  constructor(documentCount: number) {
+    this.documentCount = documentCount
+
+    if (this.documentCount > vocabularyItems.length) {
+      throw new Error(`Only ${vocabularyItems.length} documents can be created`)
+    }
+  }
+
+  build(): Array<VocabularyItem> {
+    return vocabularyItems.slice(0, this.documentCount)
+  }
+}
+
+export default VocabularyItemBuilder
