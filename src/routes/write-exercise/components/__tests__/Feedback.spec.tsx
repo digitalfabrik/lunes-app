43a0e97de7da94042e56d5878c98e5a92a5494ac
lunars97 <alekbarova.aizhana@gmail.com>
2022-10-13 14:21:22 +0200

    LUN-417-Remove-Sentence-when-solution-is-shown

--- src/routes/write-exercise/components/__tests__/Feedback.spec.tsx
+++ src/routes/write-exercise/components/__tests__/Feedback.spec.tsx
@@ -18,27 +18,20 @@ describe('Feedback', () => {
     word: 'Abrissbirne',
   }
 
-  const renderFeedback = (
-    result: SimpleResult,
-    numberOfTries: number,
-    submission: string,
-    needsToBeRepeated: boolean
-  ): RenderAPI => {
+  const renderFeedback = (result: SimpleResult, numberOfTries: number, submission: string): RenderAPI => {
     const documentWithResult: DocumentResult = { document, result, numberOfTries }
-    return render(
-      <Feedback documentWithResult={documentWithResult} submission={submission} needsToBeRepeated={needsToBeRepeated} />
-    )
+    return render(<Feedback documentWithResult={documentWithResult} submission={submission} />)
   }
 
   it('should render correct feedback', () => {
     const submission = 'Die Abrissbirne'
-    const { queryByText } = renderFeedback('correct', 1, submission, false)
+    const { queryByText } = renderFeedback('correct', 1, submission)
     expect(queryByText(getLabels().exercises.write.feedback.correct.replace('\n', ''))).toBeTruthy()
   })
 
   it('should render similar feedback', () => {
     const submission = 'Die Abrissbirn'
-    const { queryByText } = renderFeedback('similar', 1, submission, true)
+    const { queryByText } = renderFeedback('similar', 1, submission)
 
     expect(
       queryByText(
@@ -51,7 +44,7 @@ describe('Feedback', () => {
 
   it('should render finally incorrect feedback', () => {
     const submission = 'Der Auto'
-    const { queryByText } = renderFeedback('incorrect', 1, submission, false)
+    const { queryByText } = renderFeedback('incorrect', 1, submission)
 
     expect(
       queryByText(
@@ -62,7 +55,7 @@ describe('Feedback', () => {
 
   it('should render incorrect feedback with retries not exceeded', () => {
     const submission = 'Der Auto'
-    const { queryByText } = renderFeedback('incorrect', 1, submission, true)
+    const { queryByText } = renderFeedback('incorrect', 1, submission)
 
     expect(
       queryByText(
