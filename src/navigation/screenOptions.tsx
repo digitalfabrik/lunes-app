b3e9d19ad2402b06a0f64c9e40e425e5b4dcd9cb
bob606 <saliu.bah654@gmail.com>
2022-10-25 15:32:42 +0200

    refactor: reverted screen options

--- src/navigation/screenOptions.tsx
+++ src/navigation/screenOptions.tsx
@@ -9,51 +9,49 @@ import theme from '../constants/theme'
 import { COLORS } from '../constants/theme/colors'
 import { RoutesParams } from './NavigationTypes'
 
-export const headerHeightPercentage = hp('15%')
-
 export const useTabletHeaderHeight = (): number | undefined => {
-  const { width } = useWindowDimensions()
-  const MOBILE_MAX_WIDTH = 550
-  return width > MOBILE_MAX_WIDTH ? hp('7%') : undefined
+    const { width } = useWindowDimensions()
+    const MOBILE_MAX_WIDTH = 550
+    return width > MOBILE_MAX_WIDTH ? hp('7%') : undefined
 }
 
 const headerStyles = (headerHeight?: number) =>
-  StyleSheet.create({
-    header: {
-      backgroundColor: COLORS.background,
-      shadowOpacity: 0,
-      elevation: 0,
-      borderBottomColor: COLORS.disabled,
-      borderBottomWidth: 1,
-      height: headerHeight,
-    },
-    headerRightContainer: {
-      paddingHorizontal: theme.spacingsPlain.sm,
-      maxWidth: 60,
-    },
-    headerLeftContainer: {
-      flex: 1,
-      padding: 0,
-    },
-    headerTitleContainer: {
-      marginHorizontal: 0,
-    },
-  })
+    StyleSheet.create({
+        header: {
+            backgroundColor: COLORS.background,
+            shadowOpacity: 0,
+            elevation: 0,
+            borderBottomColor: COLORS.disabled,
+            borderBottomWidth: 1,
+            height: headerHeight,
+        },
+        headerRightContainer: {
+            paddingHorizontal: theme.spacingsPlain.sm,
+            maxWidth: 60,
+        },
+        headerLeftContainer: {
+            flex: 1,
+            padding: 0,
+        },
+        headerTitleContainer: {
+            marginHorizontal: 0,
+        },
+    })
 
 const screenOptions =
-  (headerHeight?: number) =>
-  (title: string, navigation: NavigationProp<RoutesParams>, isCloseButton = false): StackNavigationOptions => {
-    const { header, headerLeftContainer, headerRightContainer, headerTitleContainer } = headerStyles(headerHeight)
-    return {
-      headerLeft: () => (
-        <NavigationHeaderLeft onPress={navigation.goBack} title={title} isCloseButton={isCloseButton} />
-      ),
-      headerTitle: '',
-      headerStyle: header,
-      headerRightContainerStyle: headerRightContainer,
-      headerLeftContainerStyle: headerLeftContainer,
-      headerTitleContainerStyle: headerTitleContainer,
-    }
-  }
+    (headerHeight?: number) =>
+        (title: string, navigation: NavigationProp<RoutesParams>, isCloseButton = false): StackNavigationOptions => {
+            const { header, headerLeftContainer, headerRightContainer, headerTitleContainer } = headerStyles(headerHeight)
+            return {
+                headerLeft: () => (
+                    <NavigationHeaderLeft onPress={navigation.goBack} title={title} isCloseButton={isCloseButton} />
+                ),
+                headerTitle: '',
+                headerStyle: header,
+                headerRightContainerStyle: headerRightContainer,
+                headerLeftContainerStyle: headerLeftContainer,
+                headerTitleContainerStyle: headerTitleContainer,
+            }
+        }
 
 export default screenOptions
