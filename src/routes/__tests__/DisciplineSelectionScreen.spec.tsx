c82293aab4d001592aa813b23f3bba8e80dce2b0
Cmd190 <tonigaser@gmail.com>
2022-11-18 10:34:01 +0100

    LUN-181: fixed unit test

--- src/routes/__tests__/DisciplineSelectionScreen.spec.tsx
+++ src/routes/__tests__/DisciplineSelectionScreen.spec.tsx
@@ -69,7 +69,7 @@ describe('DisciplineSelectionScreen', () => {
       discipline: mockDisciplines()[2],
       disciplineTitle: mockDisciplines()[2].title,
       disciplineId: mockDisciplines()[2].id,
-      documents: null,
+      "vocabularyItems": null,
     })
   })
 
