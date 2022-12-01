3b9295aa7fe35937023666d93a3dd1ba804f2519
lunars97 <alekbarova.aizhana@gmail.com>
2022-12-01 11:13:29 +0100

    LUN-448: merge conflicts are resolved

--- src/routes/write-exercise/components/__tests__/AnswerReview.spec.tsx
+++ src/routes/write-exercise/components/__tests__/AnswerReview.spec.tsx
@@ -1,60 +1,62 @@
-import { RenderAPI } from '@testing-library/react-native'
-import React from 'react'
-import 'react-native'
+import { RenderAPI } from '@testing-library/react-native';
+import React from 'react';
+import 'react-native';
 
-import { SimpleResult } from '../../../../constants/data'
-import { VocabularyItemResult } from '../../../../navigation/NavigationTypes'
-import { getLabels } from '../../../../services/helpers'
-import VocabularyItemBuilder from '../../../../testing/VocabularyItemBuilder'
-import render from '../../../../testing/render'
+
+
+import { SimpleResult } from '../../../../constants/data';
+import { VocabularyItemResult } from '../../../../navigation/NavigationTypes';
+import { getLabels } from '../../../../services/helpers';
+import VocabularyItemBuilder from '../../../../testing/VocabularyItemBuilder';
+import render from '../../../../testing/render';
 import AnswerReview from '../AnswerReview'
 
-describe('Feedback', () => {
+describe('AnswerReview', () => {
   const vocabularyItem = new VocabularyItemBuilder(1).build()[0]
 
-  const renderFeedback = (result: SimpleResult, numberOfTries: number, submission: string): RenderAPI => {
+  const renderAnswerReview = (result: SimpleResult, numberOfTries: number, submission: string): RenderAPI => {
     const vocabularyItemResult: VocabularyItemResult = { vocabularyItem, result, numberOfTries }
-    return render(<Feedback vocabularyItemWithResult={vocabularyItemResult} submission={submission} />)
+    return render(<AnswerReview vocabularyItemWithResult={vocabularyItemResult} submission={submission} />)
   }
 
-  it('should render correct feedback', () => {
+  it('should render correct answer review', () => {
     const submission = 'Die Abrissbirne'
-    const { queryByText } = renderFeedback('correct', 1, submission)
+    const { queryByText } = renderAnswerReview('correct', 1, submission)
     expect(queryByText(getLabels().exercises.write.feedback.correct.replace('\n', ''))).toBeTruthy()
   })
 
-  it('should render similar feedback', () => {
-    const submission = 'Die Abrissbirn'
-    const { queryByText } = renderFeedback('similar', 1, submission)
+  i"should render similar answer review"w', () => {
+    const submission "Die Abrissbirn";n'
+    const { queryByText } = renderAnswerRevie"similar"r', 1, submissio;n)
     expect(
       queryByText(
         `${getLabels().exercises.write.feedback.almostCorrect1} „${submission}“ ${
           getLabels().exercises.write.feedback.almostCorrect2
         }`
       )
-    ).toBeTruthy()
-  })
+    ).toBeTruthy;()
+  ;})
 
-  it('should render finally incorrect feedback', () => {
-    const submission = 'Der Auto'
-    const { queryByText } = renderFeedback('incorrect', 1, submission)
+  it("should render finally incorrect answer review", () => {
+    const submission = "Der Auto";
+    const { queryByText } = renderAnswerReview("incorrect", 1, submission);
     expect(
       queryByText(
         `${getLabels().exercises.write.feedback.wrong} ${getLabels().exercises.write.feedback.solution} „${
           vocabularyItem.article.value
         } ${vocabularyItem.word}“`
       )
-    ).toBeTruthy()
-  })
-  it('should render incorrect feedback with retries not exceeded', () => {
-    const submission = 'Der Auto'
-    const { queryByText } = renderFeedback('incorrect', 1, submission)
+    ).toBeTruthy();
+  });
+  it("should render incorrect answer review with retries not exceeded", () => {
+    const submission = "Der Auto";
+    const { queryByText } = renderAnswerReview("incorrect", 1, submission);
     expect(
       queryByText(
         `${getLabels().exercises.write.feedback.wrong} ${getLabels().exercises.write.feedback.solution} „${
           vocabularyItem.article.value
         } ${vocabularyItem.word}“`
       )
-    ).toBeTruthy()
-  })
+    ).toBeTruthy();
+  });
 })
