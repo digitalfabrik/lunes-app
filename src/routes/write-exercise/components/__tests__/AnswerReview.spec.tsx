06398797a8cde70e478ee699101335d7091164f7
lunars97 <alekbarova.aizhana@gmail.com>
2022-12-01 11:24:33 +0100

    LUN-448: Merge conflicts are resolved

--- src/routes/write-exercise/components/__tests__/AnswerReview.spec.tsx
+++ src/routes/write-exercise/components/__tests__/AnswerReview.spec.tsx
@@ -1,14 +1,12 @@
-import { RenderAPI } from '@testing-library/react-native';
-import React from 'react';
-import 'react-native';
+import { RenderAPI } from '@testing-library/react-native'
+import React from 'react'
+import 'react-native'
 
-
-
-import { SimpleResult } from '../../../../constants/data';
-import { VocabularyItemResult } from '../../../../navigation/NavigationTypes';
-import { getLabels } from '../../../../services/helpers';
-import VocabularyItemBuilder from '../../../../testing/VocabularyItemBuilder';
-import render from '../../../../testing/render';
+import { SimpleResult } from '../../../../constants/data'
+import { VocabularyItemResult } from '../../../../navigation/NavigationTypes'
+import { getLabels } from '../../../../services/helpers'
+import VocabularyItemBuilder from '../../../../testing/VocabularyItemBuilder'
+import render from '../../../../testing/render'
 import AnswerReview from '../AnswerReview'
 
 describe('AnswerReview', () => {
@@ -25,38 +23,38 @@ describe('AnswerReview', () => {
     expect(queryByText(getLabels().exercises.write.feedback.correct.replace('\n', ''))).toBeTruthy()
   })
 
-  i"should render similar answer review"w', () => {
-    const submission "Die Abrissbirn";n'
-    const { queryByText } = renderAnswerRevie"similar"r', 1, submissio;n)
+  it('should render similar answer review', () => {
+    const submission = 'Die Abrissbirn'
+    const { queryByText } = renderAnswerReview('similar', 1, submission)
     expect(
       queryByText(
         `${getLabels().exercises.write.feedback.almostCorrect1} „${submission}“ ${
           getLabels().exercises.write.feedback.almostCorrect2
         }`
       )
-    ).toBeTruthy;()
-  ;})
+    ).toBeTruthy()
+  })
 
-  it("should render finally incorrect answer review", () => {
-    const submission = "Der Auto";
-    const { queryByText } = renderAnswerReview("incorrect", 1, submission);
+  it('should render finally incorrect answer review', () => {
+    const submission = 'Der Auto'
+    const { queryByText } = renderAnswerReview('incorrect', 1, submission)
     expect(
       queryByText(
         `${getLabels().exercises.write.feedback.wrong} ${getLabels().exercises.write.feedback.solution} „${
           vocabularyItem.article.value
         } ${vocabularyItem.word}“`
       )
-    ).toBeTruthy();
-  });
-  it("should render incorrect answer review with retries not exceeded", () => {
-    const submission = "Der Auto";
-    const { queryByText } = renderAnswerReview("incorrect", 1, submission);
+    ).toBeTruthy()
+  })
+  it('should render incorrect answer review with retries not exceeded', () => {
+    const submission = 'Der Auto'
+    const { queryByText } = renderAnswerReview('incorrect', 1, submission)
     expect(
       queryByText(
         `${getLabels().exercises.write.feedback.wrong} ${getLabels().exercises.write.feedback.solution} „${
           vocabularyItem.article.value
         } ${vocabularyItem.word}“`
       )
-    ).toBeTruthy();
-  });
+    ).toBeTruthy()
+  })
 })
