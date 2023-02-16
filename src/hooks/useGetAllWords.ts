1e2898cb936e98faa359d475a98a1a62c6d7386c
steffi <metzger@integreat-app.de>
2023-02-16 10:51:53 +0100

    LUN-435: Use spread operator

--- src/hooks/useGetAllWords.ts
+++ src/hooks/useGetAllWords.ts
@@ -6,12 +6,7 @@ import { Return, useLoadAsync } from './useLoadAsync'
 export const getAllWords = async (): Promise<VocabularyItem[]> => {
   const lunesStandardVocabulary = loadAllVocabularyItems()
   const userVocabulary = getUserVocabularyItems()
-  let response: VocabularyItem[] = []
-  return Promise.all([lunesStandardVocabulary, userVocabulary]).then(values => {
-    response = response.concat(values[0])
-    response = response.concat(values[1])
-    return response
-  })
+  return Promise.all([lunesStandardVocabulary, userVocabulary]).then(values => [...values[0], ...values[1]])
 }
 
 const useGetAllWords = (): Return<VocabularyItem[]> => useLoadAsync(getAllWords, {})
