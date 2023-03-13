1edac6b05fb4fbf7fb092292805f9581e84f2983
Leandra Hahn <LeandraH@users.noreply.github.com>
2023-03-13 16:35:34 +0100

    LUN-440: Select image from gallery (#420)
    * LUN-440: Select image from gallery
    
    * LUN-440: WIP
    
    * LUN-440: Use react-native-image-crop-picker
    
    * LUN-440: Clean up
    
    * LUN-440: Restore old yarn.lock
    
    * LUN-440: Prettier
    
    * LUN-440: Fix Test
    
    * LUN-440: Fix Typo
    
    * LUN-440: Remove provisioning profile specifier
    
    * LUN-440: Remove additional development team
    
    * LUN-440: Update XCode version
    
    * LUN-440: Downgrade pods
    
    * LUN-440: Revert changes to ios project file
    
    * LUN-440: Update rn-permissions
    
    * LUN-440: Update rn-permissions everywhere
    
    * LUN-440: Add signing for XCode 14
    
    * LUN-440: Remove release note
    
    * LUN-440: Remove magic number

--- src/routes/process-user-vocabulary/components/ImageSelectionOverlay.tsx
+++ src/routes/process-user-vocabulary/components/ImageSelectionOverlay.tsx
@@ -1,10 +1,16 @@
-import React, { ReactElement, useRef } from 'react'
+import React, { ReactElement, useRef, useState } from 'react'
+import { Platform } from 'react-native'
 import { RNCamera } from 'react-native-camera'
+import { openPicker } from 'react-native-image-crop-picker'
+import { PERMISSIONS } from 'react-native-permissions'
 import styled from 'styled-components/native'
 
 import { CircleIconWhite, ImageIcon } from '../../../../assets/images'
 import CameraOverlay from '../../../components/CameraOverlay'
+import NotAuthorisedView from '../../../components/NotAuthorisedView'
 import PressableOpacity from '../../../components/PressableOpacity'
+import { getLabels } from '../../../services/helpers'
+import { reportError } from '../../../services/sentry'
 
 const GALLERY_ICON_SIZE = 30
 const TAKE_IMAGE_ICON_SIZE = 50
@@ -40,8 +46,10 @@ type ImageSelectionOverlayProps = {
 
 const ImageSelectionOverlay = ({ setVisible, pushImage }: ImageSelectionOverlayProps): ReactElement => {
   const camera = useRef<RNCamera>(null)
+  const [isGalleryOpen, setIsGalleryOpen] = useState<boolean>(false)
+  const [showNotAuthorised, setShowNotAuthorised] = useState<boolean>(false)
 
-  const takeImage = async () => {
+  const takePhoto = async () => {
     if (camera.current) {
       const options = { quality: 0.5, base64: true }
       const data = await camera.current.takePictureAsync(options)
@@ -50,17 +58,56 @@ const ImageSelectionOverlay = ({ setVisible, pushImage }: ImageSelectionOverlayP
     }
   }
 
+  const openGallery = () => {
+    setIsGalleryOpen(true)
+    openPicker({
+      mediaType: 'photo',
+    })
+      .then(image => {
+        setVisible(false)
+        pushImage(image.path)
+      })
+      .catch((e: Error) => {
+        if (e.message.includes('permission')) {
+          setShowNotAuthorised(true)
+        } else if (!e.message.includes('cancelled')) {
+          reportError(e)
+        }
+      })
+      .finally(() => {
+        setIsGalleryOpen(false)
+      })
+  }
+
+  const firstAndroidVersionWithSplitPermissions = 33
+  const androidMediaPermission =
+    Platform.Version >= firstAndroidVersionWithSplitPermissions
+      ? PERMISSIONS.ANDROID.READ_MEDIA_IMAGES
+      : PERMISSIONS.ANDROID.READ_EXTERNAL_STORAGE
+
+  let permission
+  if (isGalleryOpen) {
+    permission = Platform.OS === 'ios' ? PERMISSIONS.IOS.PHOTO_LIBRARY : androidMediaPermission
+  }
+
+  if (showNotAuthorised) {
+    return (
+      <NotAuthorisedView
+        description={getLabels().general.library.noAuthorization.description}
+        setVisible={setShowNotAuthorised}
+      />
+    )
+  }
+
   return (
-    <CameraOverlay setVisible={setVisible}>
+    <CameraOverlay setVisible={setVisible} permission={permission}>
       <Camera ref={camera} captureAudio={false} testID='camera'>
         <Container>
           <ActionBar>
-            {/* TODO LUN-440 implement gallery */}
-            {/* eslint-disable-next-line no-console */}
-            <PressableOpacity onPress={() => console.log('open gallery')}>
+            <PressableOpacity onPress={openGallery}>
               <ImageIcon width={GALLERY_ICON_SIZE} height={GALLERY_ICON_SIZE} testID='gallery-icon' />
             </PressableOpacity>
-            <TakeImageButton onPress={takeImage}>
+            <TakeImageButton onPress={takePhoto}>
               <CircleIconWhite width={TAKE_IMAGE_ICON_SIZE} height={TAKE_IMAGE_ICON_SIZE} testID='take-image-icon' />
             </TakeImageButton>
           </ActionBar>
