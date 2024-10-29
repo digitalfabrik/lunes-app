a7637e4b2e2d9c527fd4b6834265e1f2adbc1846
Steffen Kleinle <steffen.kleinle@mailbox.org>
2024-10-29 12:11:37 +0100

    1010: Upgrade to react-native 0.72

--- android/app/src/main/java/app/lunes/MainApplication.java
+++ android/app/src/main/java/app/lunes/MainApplication.java
@@ -1,7 +1,6 @@
 package app.lunes;
 
 import android.app.Application;
-import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.rnfs.RNFSPackage;
@@ -12,21 +11,19 @@ import com.johnsonsu.rnsoundplayer.RNSoundPlayerPackage;
 import net.no_mad.tts.TextToSpeechPackage;
 import net.no_mad.tts.TextToSpeechPackage;
 import com.horcrux.svg.SvgPackage;
-import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
-import com.facebook.react.config.ReactFeatureFlags;
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
+import com.facebook.react.defaults.DefaultReactNativeHost;
 import com.facebook.soloader.SoLoader;
-import app.lunes.newarchitecture.MainApplicationReactNativeHost;
 import com.facebook.react.bridge.JSIModulePackage;
 import com.swmansion.reanimated.ReanimatedJSIModulePackage;
-import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost =
-      new ReactNativeHost(this) {
+      new DefaultReactNativeHost(this) {
           @Override
           public boolean getUseDeveloperSupport() {
               return BuildConfig.DEBUG;
@@ -47,6 +44,16 @@ public class MainApplication extends Application implements ReactApplication {
           }
 
           @Override
+          protected boolean isNewArchEnabled() {
+              return BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
+          }
+
+          @Override
+          protected Boolean isHermesEnabled() {
+              return BuildConfig.IS_HERMES_ENABLED;
+          }
+
+          @Override
           protected JSIModulePackage getJSIModulePackage() {
               return new ReanimatedJSIModulePackage();
           }
@@ -57,54 +64,20 @@ public class MainApplication extends Application implements ReactApplication {
 
   @Override
   public ReactNativeHost getReactNativeHost() {
-    if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-      return mNewArchitectureNativeHost;
-    } else {
       return mReactNativeHost;
-    }
   }
 
   @Override
   public void onCreate() {
     super.onCreate();
-    // If you opted-in for the New Architecture, we enable the TurboModule system
-    ReactFeatureFlags.useTurboModules = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
     SoLoader.init(this, /* native exopackage */ false);
-    initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
 
     // FORCE LTR
     I18nUtil sharedI18nUtilInstance = I18nUtil.getInstance();
     sharedI18nUtilInstance.allowRTL(getApplicationContext(), false);
-  }
-
-  /**
-   * Loads Flipper in React Native templates. Call this in the onCreate method with something like
-   * initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
-   *
-   * @param context
-   * @param reactInstanceManager
-   */
-  private static void initializeFlipper(
-      Context context, ReactInstanceManager reactInstanceManager) {
-    if (BuildConfig.DEBUG) {
-      try {
-        /*
-         We use reflection here to pick up the class that initializes Flipper,
-        since Flipper library is not available in release mode
-        */
-        Class<?> aClass = Class.forName("app.lunes.ReactNativeFlipper");
-        aClass
-            .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
-            .invoke(null, context, reactInstanceManager);
-      } catch (ClassNotFoundException e) {
-        e.printStackTrace();
-      } catch (NoSuchMethodException e) {
-        e.printStackTrace();
-      } catch (IllegalAccessException e) {
-        e.printStackTrace();
-      } catch (InvocationTargetException e) {
-        e.printStackTrace();
-      }
+    if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
+        // If you opted-in for the New Architecture, we load the native entry point for this app.
+        DefaultNewArchitectureEntryPoint.load();
     }
   }
 }
