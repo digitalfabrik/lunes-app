3ea2a0507cf96d9aa739720547a0e88aabfacdf5
Steffen Kleinle <steffen.kleinle@mailbox.org>
2025-01-20 11:10:02 +0100

    Upgrade react-native-reanimated to fix iOS builds

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
-  plugins: ['react-native-reanimated/plugin', '@babel/plugin-proposal-unicode-property-regex'],
+  plugins: ['@babel/plugin-proposal-unicode-property-regex', 'react-native-reanimated/plugin'],
 }
