a352920edf700f254b089641f8da6022ee6f5c30
LeandraH <LeandraH@users.noreply.github.com>
2025-06-18 17:44:26 +0200

    1098: Play sound on iOS even if the device is in silent mode

--- ios/Lunes/AppDelegate.mm
+++ ios/Lunes/AppDelegate.mm
@@ -1,11 +1,27 @@
 #import "AppDelegate.h"
 
+#import <AVFoundation/AVFoundation.h>
 #import <React/RCTBundleURLProvider.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  // To play audio even if the device is in silent mode
+  NSError *sessionError = nil;
+  BOOL success = [[AVAudioSession sharedInstance]
+                   setCategory:AVAudioSessionCategoryPlayback
+                         error:&sessionError];
+  if (!success) {
+    NSLog(@"Error setting AVAudioSession category: %@", sessionError);
+  }
+  success = [[AVAudioSession sharedInstance]
+             setActive:YES
+                   error:&sessionError];
+  if (!success) {
+    NSLog(@"Error activating AVAudioSession: %@", sessionError);
+  }
+
   self.moduleName = @"Lunes";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
