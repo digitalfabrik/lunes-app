ab49de954c06c8d0bc53c9a0767088ca95690364
David Venhoff <david.venhoff@tuerantuer.org>
2025-06-27 17:05:13 +0200

    Adjust tests

--- src/services/__tests__/url.spec.ts
+++ src/services/__tests__/url.spec.ts
@@ -11,10 +11,9 @@ jest.mock('react-native', () => ({
 }))
 
 jest.mock('react-native-responsive-screen', () => ({
-  heightPercentageToDP: jest.fn((value) => value),
-  widthPercentageToDP: jest.fn((value) => value),
+  heightPercentageToDP: jest.fn(value => value),
+  widthPercentageToDP: jest.fn(value => value),
 }))
-
 
 describe('url', () => {
   it('should successfully open an url', async () => {
