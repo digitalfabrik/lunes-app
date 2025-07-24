3d2772ebe75c9027b9c7fbe434f571bfba2c443b
David Venhoff <david.venhoff@tuerantuer.org>
2025-07-24 17:19:42 +0200

    Extract the filtered profession list into a function

--- src/routes/scope-selection/ScopeSelection.tsx
+++ src/routes/scope-selection/ScopeSelection.tsx
@@ -9,7 +9,7 @@ import { Discipline } from '../../constants/endpoints'
 import { formatDiscipline } from '../../hooks/helpers'
 import { useLoadAllDisciplines } from '../../hooks/useLoadAllDisciplines'
 import { useLoadDisciplines } from '../../hooks/useLoadDisciplines'
-import { useStorageCache } from '../../hooks/useStorage'
+import useStorage, { useStorageCache } from '../../hooks/useStorage'
 import { getLabels, searchProfessions, splitTextBySearchString } from '../../services/helpers'
 import { pushSelectedProfession, removeSelectedProfession } from '../../services/storageUtils'
 
@@ -45,25 +45,48 @@ const highlightText = (textArray: [string] | [string, string, string], disabled:
   </HighlightContainer>
 )
 
-type ScopeSelectionProps = {
+type FilteredProfessionListProps = {
   queryTerm: string
-  setQueryTerm: (newString: string) => void
-  navigateToDiscipline: (item: Discipline) => void
-  selectedProfessions: Readonly<number[]> | null
 }
 
-const ScopeSelection = ({
-  queryTerm,
-  setQueryTerm,
-  navigateToDiscipline,
-  selectedProfessions,
-}: ScopeSelectionProps): JSX.Element => {
+const FilteredProfessionList = ({ queryTerm }: FilteredProfessionListProps): JSX.Element => {
   const storageCache = useStorageCache()
-  const { data: disciplines, error, loading, refresh } = useLoadDisciplines({ parent: null })
+  const [selectedProfessions] = useStorage('selectedProfessions')
   const allProfessions = useLoadAllDisciplines()
     .data?.filter(discipline => discipline.total_discipline_children === 0)
     .map(discipline => formatDiscipline(discipline, {}))
   const filteredProfessions = useMemo(() => searchProfessions(allProfessions, queryTerm), [allProfessions, queryTerm])
+
+  return (
+    <ScopeContainer>
+      {filteredProfessions?.map(profession => {
+        const disabled = !!selectedProfessions?.includes(profession.id)
+        return (
+          <StyledPressable
+            key={profession.id}
+            onPress={async () => {
+              if (selectedProfessions?.includes(profession.id)) {
+                await removeSelectedProfession(storageCache, profession.id)
+              } else {
+                await pushSelectedProfession(storageCache, profession.id)
+              }
+            }}>
+            {highlightText(splitTextBySearchString(profession.title, queryTerm), disabled)}
+          </StyledPressable>
+        )
+      })}
+    </ScopeContainer>
+  )
+}
+
+type ScopeSelectionProps = {
+  queryTerm: string
+  setQueryTerm: (newString: string) => void
+  navigateToDiscipline: (item: Discipline) => void
+}
+
+const ScopeSelection = ({ queryTerm, setQueryTerm, navigateToDiscipline }: ScopeSelectionProps): JSX.Element => {
+  const { data: disciplines, error, loading, refresh } = useLoadDisciplines({ parent: null })
   const theme = useTheme()
 
   const disciplineItems = disciplines?.map(item => (
@@ -83,24 +106,7 @@ const ScopeSelection = ({
         />
       </SearchContainer>
       {queryTerm.length > 0 ? (
-        <ScopeContainer>
-          {filteredProfessions?.map(profession => {
-            const disabled = !!selectedProfessions?.includes(profession.id)
-            return (
-              <StyledPressable
-                key={profession.id}
-                onPress={async () => {
-                  if (selectedProfessions?.includes(profession.id)) {
-                    await removeSelectedProfession(storageCache, profession.id)
-                  } else {
-                    await pushSelectedProfession(storageCache, profession.id)
-                  }
-                }}>
-                {highlightText(splitTextBySearchString(profession.title, queryTerm), disabled)}
-              </StyledPressable>
-            )
-          })}
-        </ScopeContainer>
+        <FilteredProfessionList queryTerm={queryTerm} />
       ) : (
         <ServerResponseHandler error={error} loading={loading} refresh={refresh}>
           <DisciplineContainer>{disciplineItems}</DisciplineContainer>
