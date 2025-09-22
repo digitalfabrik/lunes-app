8524302bb92f96f8acacfb7d18d9b23fd5531e08
David Venhoff <david.venhoff@tuerantuer.org>
2025-09-22 16:47:52 +0200

    Remove suggest remove alternative word button
    Alternative words can still be suggested via the context menu,
    but the button was too confusing for users.
    
    - Don't show the button anymore
    - Completely hide the `AlternativeWordSection` if no alternatives exist

--- src/routes/vocabulary-detail-exercise/components/AlternativeWordsSection.tsx
+++ src/routes/vocabulary-detail-exercise/components/AlternativeWordsSection.tsx
@@ -1,13 +1,10 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { heightPercentageToDP as hp } from 'react-native-responsive-screen'
 import styled from 'styled-components/native'
 
-import { ArrowRightIcon, CrystalBallIcon } from '../../../../assets/images'
-import FeedbackModal from '../../../components/FeedbackModal'
-import { ContentSecondary, ContentTextBold } from '../../../components/text/Content'
-import { FeedbackType } from '../../../constants/data'
+import { CrystalBallIcon } from '../../../../assets/images'
+import { ContentSecondary } from '../../../components/text/Content'
 import { VocabularyItem } from '../../../constants/endpoints'
-import theme from '../../../constants/theme'
 import { getLabels } from '../../../services/helpers'
 
 const Heading = styled(ContentSecondary)`
@@ -28,48 +25,21 @@ const Content = styled.View`
   padding: 0 ${props => props.theme.spacings.sm};
 `
 
-const SuggestionPressable = styled.Pressable`
-  flex-direction: row;
-  align-items: center;
-  padding: ${props => props.theme.spacings.xs} 0 ${props => props.theme.spacings.xxl};
-`
-
-const Label = styled(ContentTextBold)`
-  text-transform: uppercase;
-`
-
 type AlternativeWordsSectionProps = {
   vocabularyItem: VocabularyItem
 }
 
-const AlternativeWordsSection = ({ vocabularyItem }: AlternativeWordsSectionProps): JSX.Element => {
-  const [isFeedbackModalVisible, setIsFeedbackModalVisible] = useState(false)
-  return (
+const AlternativeWordsSection = ({ vocabularyItem }: AlternativeWordsSectionProps): JSX.Element | null =>
+  vocabularyItem.alternatives.length > 0 ? (
     <Root>
       <CrystalBallIcon width={hp('3.5%')} height={hp('3.5%')} />
       <Content>
-        {vocabularyItem.alternatives.length > 0 && (
-          <>
-            <Heading>{getLabels().exercises.vocabularyList.alternativeWords}</Heading>
-            <AlternativeWords>
-              {vocabularyItem.alternatives.map(value => `${value.article.value} ${value.word}`).join(', ')}
-            </AlternativeWords>
-          </>
-        )}
-
-        <SuggestionPressable onPress={() => setIsFeedbackModalVisible(true)}>
-          <Label>{getLabels().exercises.vocabularyList.suggestAlternative}</Label>
-          <ArrowRightIcon fill={theme.colors.black} width={hp('3%')} height={hp('3%')} />
-        </SuggestionPressable>
+        <Heading>{getLabels().exercises.vocabularyList.alternativeWords}</Heading>
+        <AlternativeWords>
+          {vocabularyItem.alternatives.map(value => `${value.article.value} ${value.word}`).join(', ')}
+        </AlternativeWords>
       </Content>
-      <FeedbackModal
-        visible={isFeedbackModalVisible}
-        onClose={() => setIsFeedbackModalVisible(false)}
-        feedbackType={FeedbackType.vocabularyItem}
-        feedbackForId={vocabularyItem.id}
-      />
     </Root>
-  )
-}
+  ) : null
 
 export default AlternativeWordsSection
