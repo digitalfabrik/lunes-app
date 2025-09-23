d16178c6ba383a2481492e2dfe3307717e4ef44d
David Venhoff <david.venhoff@tuerantuer.org>
2025-09-23 16:54:02 +0200

    Add screen to remove words from long-term memory exercise

---
+++ src/routes/repetition/components/RepetitionListItem.tsx
@@ -0,0 +1,39 @@
+import React, { ReactElement } from 'react'
+import styled from 'styled-components/native'
+
+import { CloseIconRed } from '../../../../assets/images'
+import PressableOpacity from '../../../components/PressableOpacity'
+import VocabularyListItem from '../../../components/VocabularyListItem'
+import { VocabularyItem } from '../../../constants/endpoints'
+import theme from '../../../constants/theme'
+
+const Button = styled(PressableOpacity)`
+  shadow-color: ${props => props.theme.colors.shadow};
+  shadow-radius: 5px;
+  shadow-offset: 1px 1px;
+  shadow-opacity: 0.5;
+`
+
+type ListItemProps = {
+  vocabularyItem: VocabularyItem
+  navigateToDetailScreen: () => void
+  removeFromRepetition: (vocabularyItem: VocabularyItem) => void
+}
+
+const RepetitionListItem = ({
+  vocabularyItem,
+  navigateToDetailScreen,
+  removeFromRepetition,
+}: ListItemProps): ReactElement => (
+  <VocabularyListItem
+    vocabularyItem={vocabularyItem}
+    onPress={navigateToDetailScreen}
+    extraActions={
+      <Button onPress={() => removeFromRepetition(vocabularyItem)} testID='delete-button'>
+        <CloseIconRed width={theme.spacingsPlain.lg} height={theme.spacingsPlain.lg} />
+      </Button>
+    }
+  />
+)
+
+export default RepetitionListItem
--- src/services/RepetitionService.ts
+++ src/services/RepetitionService.ts
@@ -41,6 +41,19 @@ export class RepetitionService {
   public getWordNodeCard = (word: VocabularyItem): WordNodeCard | undefined =>
     this.getWordNodeCards().find(wordNodeCard => wordNodeCard.word === word)
 
+  public removeWordNodeCard = async (word: VocabularyItem): Promise<WordNodeCard | undefined> => {
+    const wordNodeCards = this.getWordNodeCards()
+      .slice()
+      .filter(wordNodeCard => wordNodeCard.word !== word)
+    if (wordNodeCards.length === this.getWordNodeCards().length) {
+      return undefined
+    }
+
+    const removed = this.getWordNodeCard(word)
+    await this.setWordNodeCards(wordNodeCards)
+    return removed
+  }
+
   private static updateRepetitionReminder = async (cards: WordNodeCard[]): Promise<void> => {
     const nextRepetitionDate = RepetitionService.getNextRepetitionDate(cards)
     if (nextRepetitionDate !== null && nextRepetitionDate > new Date()) {
