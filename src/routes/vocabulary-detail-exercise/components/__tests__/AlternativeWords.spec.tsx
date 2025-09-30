08d160519d7977f3586feba1685c45e362f3230b
David Venhoff <david.venhoff@tuerantuer.org>
2025-09-30 16:24:08 +0200

    Remove suggest remove alternative word button (#1157)
    * Remove suggest remove alternative word button
    
    Alternative words can still be suggested via the context menu,
    but the button was too confusing for users.
    
    - Don't show the button anymore
    - Completely hide the `AlternativeWordSection` if no alternatives exist
    
    * Fix tests

--- src/routes/vocabulary-detail-exercise/components/__tests__/AlternativeWords.spec.tsx
+++ src/routes/vocabulary-detail-exercise/components/__tests__/AlternativeWords.spec.tsx
@@ -16,12 +16,10 @@ describe('AlternativeWords', () => {
         `${vocabularyItems[0].alternatives[0].article.value} ${vocabularyItems[0].alternatives[0].word}, ${vocabularyItems[0].alternatives[1].article.value} ${vocabularyItems[0].alternatives[1].word}`,
       ),
     ).toBeDefined()
-    expect(getByText(getLabels().exercises.vocabularyList.suggestAlternative)).toBeDefined()
   })
 
   it('should not display alternative words, if word has none', () => {
-    const { getByText, queryByText } = render(<AlternativeWordsSection vocabularyItem={vocabularyItems[1]} />)
+    const { queryByText } = render(<AlternativeWordsSection vocabularyItem={vocabularyItems[1]} />)
     expect(queryByText(getLabels().exercises.vocabularyList.alternativeWords)).toBeNull()
-    expect(getByText(getLabels().exercises.vocabularyList.suggestAlternative)).toBeDefined()
   })
 })
