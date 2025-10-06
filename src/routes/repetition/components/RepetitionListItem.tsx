1d2a97a0203156027ec7dd67964dce7f49a35af3
David Venhoff <david.venhoff@tuerantuer.org>
2025-10-06 18:01:05 +0200

    Add confirmation modal to repetition word list

--- src/routes/repetition/components/RepetitionListItem.tsx
+++ src/routes/repetition/components/RepetitionListItem.tsx
@@ -1,11 +1,13 @@
-import React, { ReactElement } from 'react'
+import React, { ReactElement, useState } from 'react'
 import styled from 'styled-components/native'
 
 import { CloseIconRed } from '../../../../assets/images'
+import Modal from '../../../components/Modal'
 import PressableOpacity from '../../../components/PressableOpacity'
 import VocabularyListItem from '../../../components/VocabularyListItem'
 import { VocabularyItem } from '../../../constants/endpoints'
 import theme from '../../../constants/theme'
+import { getLabels } from '../../../services/helpers'
 
 const Button = styled(PressableOpacity)`
   padding-right: ${theme => theme.theme.spacings.xs};
@@ -21,16 +23,30 @@ const RepetitionListItem = ({
   vocabularyItem,
   navigateToDetailScreen,
   removeFromRepetition,
-}: ListItemProps): ReactElement => (
-  <VocabularyListItem
-    vocabularyItem={vocabularyItem}
-    onPress={navigateToDetailScreen}
-    customActions={
-      <Button onPress={() => removeFromRepetition(vocabularyItem)} testID='delete-button'>
-        <CloseIconRed width={theme.spacingsPlain.lg} height={theme.spacingsPlain.lg} />
-      </Button>
-    }
-  />
-)
+}: ListItemProps): ReactElement => {
+  const [showModal, setShowModal] = useState(false)
+  const { modalDeleteText, confirm } = getLabels().repetition.wordList
+
+  return (
+    <>
+      <Modal
+        visible={showModal}
+        onClose={() => setShowModal(false)}
+        text={modalDeleteText}
+        confirmationButtonText={confirm}
+        confirmationAction={() => removeFromRepetition(vocabularyItem)}
+      />
+      <VocabularyListItem
+        vocabularyItem={vocabularyItem}
+        onPress={navigateToDetailScreen}
+        customActions={
+          <Button onPress={() => setShowModal(true)} testID='delete-button'>
+            <CloseIconRed width={theme.spacingsPlain.lg} height={theme.spacingsPlain.lg} />
+          </Button>
+        }
+      />
+    </>
+  )
+}
 
 export default RepetitionListItem
