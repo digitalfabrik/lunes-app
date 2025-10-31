4a2d6b98f8133a79ff081a23ab2ec7c18a6f998e
David Venhoff <david.venhoff@tuerantuer.org>
2025-10-31 13:34:03 +0100

    Migrate sponsors to new api
    - Add new api endpoint in `CmsApi.ts`
    - Move the model to `models/sponsor.ts`

--- src/hooks/useGetAllSponsors.ts
+++
@@ -1,9 +0,0 @@
-import { ENDPOINTS, Sponsor } from '../constants/endpoints'
-import { getFromEndpoint } from '../services/axios'
-import { Return, useLoadAsync } from './useLoadAsync'
-
-export const getAllSponsors = async (): Promise<Sponsor[]> => getFromEndpoint<Sponsor[]>(ENDPOINTS.sponsors)
-
-const useGetAllSponsors = (): Return<Sponsor[]> => useLoadAsync(getAllSponsors, {})
-
-export default useGetAllSponsors
