4b04d7033491740f967d94c3b967e0416c42e310
David Venhoff <david.venhoff@tuerantuer.org>
2026-03-02 10:48:24 +0100

    Adapt color of the exercise progress bar

--- src/components/ExerciseHeader.tsx
+++ src/components/ExerciseHeader.tsx
@@ -137,7 +137,7 @@ const ExerciseHeader = ({
       {showProgress && (
         <ProgressBar
           animatedValue={numberOfWords > 0 ? currentWord / numberOfWords : 0}
-          color={theme.colors.progressIndicator}
+          color={theme.colors.progressIndicatorTraining}
           disabledColor={theme.colors.disabled}
         />
       )}
